1f893de59f7f204e467b28d96a0dad3ed6e976f2
Sohum Hulyalkar <ssohum11@berkeley.edu>
2021-03-01 11:54:14 -0800

    Finished A Star, still room for potential improvements, but baseline should be usable as of now.

--- src/planners/astar_planner.py
+++ src/planners/astar_planner.py
@@ -1,13 +1,17 @@
 import bisect
 
 from src.planners.planner import Planner
+from src.problem_set import ProblemSet
 from src.utils import simulation_utils
 import heapq
 import math
 import casadi as ca
 import numpy as np
 import matplotlib.pyplot as plt
+import time as t
 import pdb
+
+from src.utils.in_bounds_utils import InBounds
 
 
 class AStarPlanner(Planner):
@@ -15,31 +19,45 @@ class AStarPlanner(Planner):
 
     Attributes:
         dt:
-            A float giving the time, in seconds, between queries.
-        see Planner class for the rest of the attributes.
+            A float giving the time, in seconds, between queries. see Planner class for the rest of the attributes.
+        path:
+            A list of lon, lat coordinates for the waypoints
+        battery_levels:
+            The battery level at each waypoint
+        control:
+            List of thrusts/headings as calculated by find_min_thrust
+        failure:
+            If we failed.
     """
 
     def __init__(self, problem,
                  settings=None,
-                 t_init=None, n=100, mode='open-loop'):
+                 t_init=None, n=100, mode='open-loop', dt=10.):
         super().__init__(problem, settings, t_init, n, mode)
-        # self.dt = self.T_init / self.N
-        self.dt = 10.
+        self.dt = dt
         self.path = []
+        self.battery_levels = []
         self.times = []
         self.control = []
+        self.failure = False
+        start_time = t.time()
         self.a_star()
+        end_time = t.time()
+        print("TIME ELAPSED: ", end_time - start_time)
 
     def show_trajectory(self):
-        x_labels = list(map(lambda x: x[0], self.path))
-        y_labels = list(map(lambda x: x[1], self.path))
-        plt.scatter(x_labels, y_labels)
-        plt.annotate('x_0', (x_labels[0], y_labels[0]))
-        plt.annotate('x_T', (x_labels[-1], y_labels[-1]))
-        plt.show()
+        if not self.failure:
+            x_labels = list(map(lambda x: x[0], self.path))
+            y_labels = list(map(lambda x: x[1], self.path))
+            plt.scatter(x_labels, y_labels)
+            plt.annotate('x_0', (x_labels[0], y_labels[0]))
+            plt.annotate('x_T', (x_labels[-1], y_labels[-1]))
+            plt.show()
+        else:
+            print('failure')
 
     def get_next_action(self, state):
-        """Grab the next action. """
+        """Grab the next action. TODO: USE THE TTC """
         assert len(self.times) == len(self.control)
         assert sorted(self.times) == self.times, "The times should be in sorted order: " + str(self.times)
         idx = bisect.bisect_left(self.times, state[3])
@@ -48,13 +66,18 @@ class AStarPlanner(Planner):
             idx -= 1
         return self.control[idx]
 
+    def heuristic(self, state, target_lon, target_lat, max_velocity=0.6):
+        """ Return the minimum time between the given state and the end target """
+        distance = math.sqrt((state.lon - target_lon)**2 + (state.lat - target_lat)**2)
+        return distance * 111120. / max_velocity
+
     def a_star(self):
         """ Discretize the graph and find the time optimal path using A Star
         """
         # data structures
         time_to = {}
         edge_to = {}
-        seen = set()
+        seen = {}   # we will map each (lon, lat) to the (time, bat_level) used to get there.
         pq = []
 
         # set up problem
@@ -66,6 +89,9 @@ class AStarPlanner(Planner):
         State.set_fieldset(self.problem)
         State.time_to = time_to
         State.problem = self.problem
+        State.heuristic = lambda s: self.heuristic(s, target_lon, target_lat)
+        State.in_bounds = InBounds(self.problem.fieldset)
+        # State.heuristic = lambda s: 0
 
         starting_state = State(lon=lon, lat=lat, bat_level=1.0)
         pq.append(starting_state)
@@ -75,15 +101,30 @@ class AStarPlanner(Planner):
         end_state = State(lon=target_lon, lat=target_lat, bat_level=None)
 
         # run Dijkstra's
+        max_time = 120
+        starting_time = t.time()
         while not state == end_state:
+            if not pq or starting_time + max_time < t.time():
+                self.failure = True
+                return
             state = heapq.heappop(pq)
-            print(state.lon, state.lat, time_to[state], state.bat_level)
-            seen.add(state)
+
+            if (state.lon, state.lat) not in seen:
+                seen[(state.lon, state.lat)] = (time_to[state], state.bat_level)
+            else:
+                best_time, best_bat_level = seen[(state.lon, state.lat)]
+                seen[(state.lon, state.lat)] = min(best_time, time_to[state]), max(best_bat_level, state.bat_level)
             for time, neighbor_state in state.neighbors():
-                if neighbor_state in seen:
-                    continue
+                if (neighbor_state.lon, neighbor_state.lat) in seen:
+                    recorded_time, recorded_bat_level = seen[(neighbor_state.lon, neighbor_state.lat)]
+
+                    # if we are considering a neighbor that is worse than seen before
+                    if neighbor_state in time_to and time_to[neighbor_state] >= recorded_time \
+                            and neighbor_state.bat_level <= recorded_bat_level:
+                        continue
                 if neighbor_state not in pq:
                     pq.append(neighbor_state)
+
                 new_time = time + time_to[state]
                 if neighbor_state not in time_to or new_time < time_to[neighbor_state]:
                     time_to[neighbor_state] = new_time
@@ -91,8 +132,9 @@ class AStarPlanner(Planner):
             heapq.heapify(pq)
 
         # extract the waypoints
-        self.times, self.path, self.control = [], [], []
+        self.times, self.path, self.control, self.battery_levels = [], [], [], []
         while state != starting_state:
+            self.battery_levels.insert(0, state.bat_level)
             self.path.insert(0, (state.lon, state.lat))
             self.times.insert(0, time_to[state])
             self.control.insert(0, (state.thrust, state.heading))
@@ -112,12 +154,12 @@ class State:
         adjacent: a list of all points (lon, lat), that are adjacent to this point
 
     """
-    thrusts = [0, 0.25, 0.5, 0.75, 1.0]
-    # matrices = [], an array of matrices, one for each direction
     u_curr_func, v_curr_func = None, None
     dlon, dlat = None, None
     time_to = None
     problem = None
+    heuristic = None
+    in_bounds = None
 
     def __init__(self, lon, lat, bat_level, heading=None, thrust=None):
         self.lon = lon
@@ -128,29 +170,24 @@ class State:
         self.adjacent = self.adjacent_points()
 
     def adjacent_points(self):
-        # TODO: potentially consider 16 states instead of 8 states
         dlon, dlat = self.dlon, self.dlat
         assert dlon is not None and dlat is not None, "need to set dlon and dlat first"
         offsets = [(0, dlat), (dlon, 0), (dlon, dlat), (0, -dlat), (-dlon, 0),
                    (-dlon, -dlat), (dlon, -dlat), (-dlon, dlat)]
         for lon_offset, lat_offset in offsets:
-            yield self.lon + lon_offset, self.lat + lat_offset
+            point = (self.lon + lon_offset, self.lat + lat_offset)
+            if State.in_bounds.in_ocean(point):
+                yield point
 
-    def neighbors(self, thrust_granularity=0.1):
+    def neighbors(self):
         for lon, lat in self.adjacent:
-            # pdb.set_trace()
-            thrust, heading, time = self.find_min_thrust(lon, lat)
-            if thrust is None:
-                continue
-            # TODO: potentially add data structure that maps each state to the heading/thrust needed to get there.
-            bat_level = self.change_bat_level(thrust, time)
-            bat_level = math.floor(bat_level * 100) / 100
-            yield time, State(lon=lon, lat=lat, bat_level=bat_level, heading=heading,
-                              thrust=thrust)
-            thrust += thrust_granularity
+            for thrust, heading, time in self.actuate_towards(lon, lat):
+                bat_level = self.change_bat_level(thrust, time)
+                if bat_level >= 0.1:
+                    yield time, State(lon=lon, lat=lat, bat_level=bat_level, heading=heading, thrust=thrust)
 
     def __lt__(self, other):
-        return self.time_to[self] < self.time_to[other]
+        return self.time_to[self] + State.heuristic(self) < self.time_to[other] + State.heuristic(other)
 
     def __hash__(self):
         return hash((self.lon, self.lat, self.bat_level))
@@ -162,7 +199,10 @@ class State:
         if self.bat_level is None or other.bat_level is None:
             return True
         else:
-            return other.bat_level == self.bat_level
+            return self.bat_level_round(other.bat_level) == self.bat_level_round(self.bat_level)
+
+    def bat_level_round(self, bat_level):
+        return math.floor(bat_level * 20) / 20
 
     @classmethod
     def set_dlon_dlat(cls, lon, lat, target_lon, target_lat):
@@ -177,7 +217,7 @@ class State:
             min_thrust, heading, time
         """
         # TODO: address case when points are too close, add keyword arguments
-        starting_dlon, starting_dlat = 0.03, 0.03
+        starting_dlon, starting_dlat = 0.05, 0.05
         lon_dist, lat_dist = abs(target_lon - lon), abs(target_lat - lat)
 
         lon_intervals, lon_extra = lon_dist // starting_dlon, lon_dist % starting_dlon
@@ -193,8 +233,8 @@ class State:
         else:
             cls.dlat = lat_dist
 
-    def find_min_thrust(self, lon, lat, matrix=None, v_min=0.1, distance=None, max_thrust=1.0):
-        """ Finds the minimum thrust to go from the current state to the given lon, lat
+    def actuate_towards(self, lon, lat, matrix=None, v_min=0.1, distance=None):
+        """ Yields thrusts
 
         Args:
             lon: longitude, a float
@@ -211,6 +251,8 @@ class State:
 
         def normalize(v):
             return v / math.sqrt(np.sum(v ** 2))
+
+        max_thrust = self.problem.dyn_dict['u_max']
 
         # TODO: precompute in the future
         # Step 1:  Find the distance between the two points
@@ -242,29 +284,35 @@ class State:
         u_perp = -curr_perp
 
         # Step 5: Set magnitude of d u_parallel = max(v_min - c_parallel, 0), heading to be direction between points.
-        u_parallel = max(v_min - curr_parallel, 0)
+        u_parallel_min = max(v_min - curr_parallel, 0)
 
         # Step 6: If u_perp is more than possible thrust, failure
-        if abs(u_perp) > max_thrust or abs(u_parallel) > max_thrust:
-            return None, None, None
+        if math.sqrt(u_perp ** 2 + u_parallel_min ** 2) > max_thrust:
+            return
 
-        # Step 7: Find the TIME = distance / (u_parallel + current_parallel)
-        # TODO: replace hard coded constant with settings data reference
-        time = distance * 111120. / (u_parallel + curr_parallel)
+        u_parallel_max = math.sqrt(max_thrust**2 - u_perp**2)
+        # u_parallels = list(np.arange(u_parallel_min, u_parallel_max - 0.1, 0.1)) + [u_parallel_max]
 
-        # Step 8: Go back to original basis, i.e. in terms of u and v
-        u_vector = np.array([u_parallel, u_perp])
-        u_dir = np.matmul(u_vector, matrix)
+        # Can potentially consider more than one thrust
+        for u_parallel in [u_parallel_min]:
 
-        # Step 9: Find the thrust and heading
-        thrust_array = Planner.transform_u_dir_to_u(self=None, u_dir=u_dir)
-        thrust, heading = thrust_array[0], thrust_array[1]
+            # Step 7: Find the TIME = distance / (u_parallel + current_parallel)
+            # TODO: replace hard coded constant with settings data reference
+            time = distance * 111120. / (u_parallel + curr_parallel)
 
-        return thrust, heading, time
+            # Step 8: Go back to original basis, i.e. in terms of u and v
+            u_vector = np.array([u_parallel, u_perp])
+            u_dir = np.matmul(u_vector, matrix)
+
+            # Step 9: Find the thrust and heading
+            thrust_array = Planner.transform_u_dir_to_u(self=None, u_dir=u_dir)
+            thrust, heading = thrust_array[0], thrust_array[1]
+
+            yield thrust, heading, time
 
     def change_bat_level(self, thrust, time):
         # return self.bat_level
-        bat_level_delta = self.problem.dyn_dict['charge'] - self.problem.dyn_dict['energy'] * (self.problem.dyn_dict['u_max'] * thrust) ** 3
+        bat_level_delta = self.problem.dyn_dict['charge'] - self.problem.dyn_dict['energy'] * (thrust ** 3)
         return min(self.bat_level + time * bat_level_delta, 1)
 
     @classmethod
---
+++ src/utils/in_bounds_utils.py
@@ -0,0 +1,87 @@
+import math
+
+
+class InBounds:
+    """ Utils for checking validity of points """
+    def __init__(self, fieldset):
+        self.fieldset = fieldset
+
+    def valid_start_and_end(self, x_0, x_T):
+        """Determines whether the given start (x_0) and target (x_T) are valid.
+
+        For a start and end to be valid, they must be sufficiently far apart, and neither point can be in the ocean.
+
+        Args:
+            x_0:
+                The starting point, a pair of longitude and latitude coordinates: [lon, lat].
+            x_T:
+                The target point, a pair of longitude and latitude coordinates: [lon, lat].
+        Returns:
+            A boolean.
+        """
+        if x_0 is None or x_T is None:
+            return False
+        return self.is_far_apart(x_0, x_T) and self.in_ocean(x_0) and self.in_ocean(x_T)
+
+    def is_far_apart(self, x_0, x_T, sep=0.5):
+        """Returns whether x_0 and x_T are sufficiently far apart
+
+        Args:
+            x_0:
+                The starting point, a pair of longitude and latitude coordinates: [lon, lat].
+            x_T:
+                The target point, a pair of longitude and latitude coordinates: [lon, lat].
+            sep:
+                The minimum distance between the two points.
+        Returns:
+            A boolean.
+        """
+        lon, lat, lon_target, lat_target = x_0[0], x_0[1], x_T[0], x_T[1]
+        dlon = lon_target - lon
+        dlat = lat_target - lat
+        mag = math.sqrt(dlon * dlon + dlat * dlat)  # mag is the distance between the two points.
+        return mag > sep
+
+    def in_ocean(self, point, offset=0.1):
+        """ Returns whether the point is in the ocean.
+
+        Determines this by checking if the velocity is nonzero for this and ALL points that are "offset" distance
+        about the point in the 8 directions.
+
+        Args:
+            point:
+                A pair of longitude and latitude coordinates: [lon, lat].
+            offset: A float which determines how far about the point to look. Increasing the value of offset will
+                prevent points on the coast from being chosen.
+
+        Returns:
+            A boolean.
+        """
+
+        lon, lat = point[0], point[1]
+        degrees = [0, offset, -offset]
+        offsets = [(lon, lat) for lon in degrees for lat in degrees]
+        for lon_offset, lat_offset in offsets:
+            if self.zero_velocity(lon + lon_offset, lat + lat_offset):
+                return False
+        return True
+
+    def out_of_bounds(self, coordinate, grid):
+        """Determines whether the given coordinate (either lat or lon) is out of bounds for its respective grid.
+
+        Returns:
+            A boolean.
+        """
+        return coordinate < min(grid) or coordinate > max(grid)
+
+    def zero_velocity(self, lon, lat):
+        """Determines whether the (lon, lat) pair is zero velocity, i.e. on land.
+
+        Returns:
+            A boolean.
+        """
+        if self.out_of_bounds(lat, self.fieldset.U.grid.lat) or self.out_of_bounds(lon, self.fieldset.U.grid.lon):
+            return True
+        x = self.fieldset.U.eval(0., 0., lat, lon)
+        y = self.fieldset.V.eval(0., 0., lat, lon)
+        return x == 0. and y == 0.
