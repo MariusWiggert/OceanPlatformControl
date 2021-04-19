ba2315f6d72c558d23ed6b136b25e2019a2e3374
Sohum Hulyalkar <ssohum11@berkeley.edu>
2021-04-19 11:15:41 -0700

    Most recent changes to minimum thrust actuation logic, only looking at points ahead based on time.

--- src/trajectory_tracking_controller.py
+++ src/trajectory_tracking_controller.py
@@ -2,4 +2,15 @@ class TrajectoryTrackingController:
     """
     TODO: implement controller to go along planned trajectory for platforms.
     """
-    pass
+    def __init(self, trajectory, state):  # Will need planned trajectory and current state
+        # 0) Set up the variables
+        _L1_damping = 0.75
+        _L1_period = 17  # we should set this to dt.
+        dist_min = '?'
+        groundSpeed = "our current speed"  # shouldn't be needed, i.e. doesn't matter
+
+        # 1) calculate the L1_Gain
+        K_L1 = 4.0 * _L1_damping * _L1_damping
+
+        # 2) Calculate the L1_dist
+        _L1_dist = max(0.3183099 * _L1_damping * _L1_period * groundSpeed, dist_min);
