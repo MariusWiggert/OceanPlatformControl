c88b340ba46a77f7ad78cffd310c3da314019032
helenpxx <helenpeng2000@gmail.com>
2021-10-15 15:39:49 -0700

    testing

--- scripts/planner_example_scripts_not_working/ipopt_fix_cur.py
+++ scripts/planner_example_scripts_not_working/ipopt_fix_cur.py
@@ -1,29 +1,33 @@
+import yaml
+
 from src.utils import hycom_utils
 from src.planners.ipopt_planner import IpoptPlannerFixCur
 import os
-
 from src.problem import Problem
-from src.simulation.simulator import Simulator
+from datetime import datetime, timedelta, timezone
 
 project_dir = os.path.abspath(os.path.join(os.getcwd()))
-
-#%% Set stuff up
-nc_file = 'data/' + "gulf_of_mexico_2020-11-17_fixed_cur_small.nc"
+# Set stuff up
+nc_file = '2021_06_1-05_hourly.nc4'
 fieldset = hycom_utils.get_hycom_fieldset(nc_file)
 
 # long around the vortex
 x_0 = [-96.9, 22.8]
 x_T = [-96.9, 22.2]
-
+t_0 = datetime(2021, 6, 1, 12, 10, 10, tzinfo=timezone.utc)
 # planner fixed time horizon
 T_planner_in_h = 224
-
+# Getting parameters from yaml file
+controller_yaml = open("config/controller.yaml")
+parsed_yaml_file = yaml.load(controller_yaml, Loader=yaml.FullLoader)
+gen_settings = parsed_yaml_file["planner"]["gen_settings"]
+specific_settings= {'n_dec_var': 100, 'T_goal_in_h': 20, 'deg_around_x0_xT_box': 0.8, 'direction': 'forward','initial_set_radius': 0.1}
 # Step 1: set up problem
-prob = Problem(fieldset, x_0, x_T, project_dir, config_yaml='platform.yaml', fixed_time_index=None)
+prob = Problem(x_0, x_T, t_0, nc_file, forecast_folder="data/forecast",config_yaml='platform.yaml')
 prob.viz()
-
-#%% Step 2: initialize & run the planner
-ipopt_planner = IpoptPlannerFixCur(problem=prob, t_init_in_h=T_planner_in_h, n_dec_var=100)
+# Step 2: initialize & run the planner
+ipopt_planner = IpoptPlannerFixCur(problem=prob, gen_settings=gen_settings, specific_settings=specific_settings)
+ipopt_planner.plan(x_T)
 ipopt_planner.plot_opt_results()
 
 #%% Simulator
