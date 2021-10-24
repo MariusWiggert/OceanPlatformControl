da14bf94f8dc701c00bb5603dcbda76dcaea5964
Sunay Dagli <sunaydagli@berkeley.edu>
2021-10-23 17:57:56 -0700

    gitignore, feedback

--- viz/visualization.py
+++ viz/visualization.py
@@ -17,6 +17,27 @@ class Visualization:
     x_T = [destination longitude, destination latitude]
     nc_file_path = current only function serves as the name of the gif to be generated
     size = size to be passed into the figure size
+
+    Example Usage:
+
+    from src.utils.simulation_utils import get_current_data_subset
+
+    hindcast_file = 'notebook_example/2021_06_1-05_hourly.nc4'
+    x_0 = [-88.0, 25.0, 1, 1622549410.0]  # lon, lat, battery, posix_time
+    x_T = [-88.0, 26.3]
+    deg_around_x0_xT_box = 0.5
+    fixed_time = None
+    temporal_stride = 1
+
+    grids_dict, u_data, v_data = get_current_data_subset(hindcast_file,
+                                                         x_0, x_T,
+                                                         deg_around_x0_xT_box,
+                                                         fixed_time,
+                                                         temporal_stride)
+
+    viz = Visualization(grids_dict, u_data, v_data)
+    viz.visualize()
+    viz.animate()
     """
     def __init__(self, grids_dict, u_data, v_data, x_0=None, x_T=None, nc_file_path='Figure', size=12):
         self.grids_dict = grids_dict
@@ -30,12 +51,13 @@ class Visualization:
     def visualize(self, time_idx=0, animate=False):
         f = plt.figure()
 
-        f.set_figwidth(size)
-        f.set_figheight(size)
+        f.set_figwidth(self.size)
+        f.set_figheight(self.size)
 
         pc = ccrs.PlateCarree()
         ax = plt.axes(projection=pc)
         ax.add_feature(cfeature.LAND, zorder=0, edgecolor='black')
+        ax.gridlines(draw_labels=True, x_inline=False, y_inline=False)
 
         y_grid = self.grids_dict.get("y_grid")
         y_grid = np.flip(y_grid, axis=0)
@@ -46,23 +68,19 @@ class Visualization:
 
         start = datetime.utcfromtimestamp(self.grids_dict['t_grid'][time_idx]).strftime('%Y-%m-%d %H:%M:%S UTC')
         ax.set_title(start)
-        ax.set_xlabel('Longitude', fontsize=12)
-        ax.set_ylabel('Latitude', fontsize=12)
         magnitude = (u_data[time_idx, ...] ** 2 + v_data[time_idx, ...] ** 2) ** 0.5
-        ax.set_xticks([])  # workaround for setting axes labels
-        ax.set_yticks([])  # workaround for setting adxes labels
+
 
         colormap = cm.rainbow
         norm = Normalize()
-        norm.autoscale(magnitude)
-
         mapped_colors = colormap(norm(magnitude))
-        for i in range(len(mapped_colors)):
-            for j in range(len(mapped_colors[0])):
-                num = np.average(mapped_colors[i][j])  # average the magnitude to support color length parameter
-                mapped_colors[i][j][3] = num
+        norm.autoscale(mapped_colors)
 
-        ax.quiver(X, Y, u_data[time_idx, ...], v_data[time_idx, ...], mapped_colors[..., 3])
+        sm = cm.ScalarMappable(cmap=colormap, norm=norm)
+        sm.set_array([])
+        ax.quiver(X, Y, u_data[time_idx, ...], v_data[time_idx, ...])
+        plt.colorbar(sm)
+        I = plt.imshow(mapped_colors, extent=[np.min(X), np.max(X), np.min(Y), np.max(Y)], cmap=colormap, norm=norm)
 
         if self.x_0 is not None and self.x_T is not None:
             plt.scatter([self.x_0[0], self.x_T[0]], [self.x_0[1], self.x_T[1]])
@@ -83,10 +101,11 @@ class Visualization:
             os.mkdir('gifs')
         if os.path.isdir('temp_photos'):
             shutil.rmtree('temp_photos')
-        os.mkdir('temp_photos')
+        png_dir = 'temp_photos'
+        os.mkdir(png_dir)
         for i in range(len(self.u_data)):
             self.visualize(time_idx=i, animate=True)
-        png_dir = 'temp_photos'
+        print('Done visualizing data over ' + str(len(self.u_data)) + ' images')
         images = []
         for file_name in sorted(os.listdir(png_dir)):
             if file_name.endswith('.png'):
@@ -96,4 +115,5 @@ class Visualization:
                 except (IOError, SyntaxError) as e:
                     print('Bad file:', file_name)
         imageio.mimsave('gifs/' + self.plot_name + '.gif', images)
+        print('Done animating data in gifs/')
         shutil.rmtree('temp_photos')
