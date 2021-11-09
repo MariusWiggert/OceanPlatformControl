bb7887dc21b9b145e646bb3731a801826f024a48
MariusWiggert <mariuswiggert@berkeley.edu>
2021-11-08 18:42:01 -0800

    do my edits change anything?

--- ocean_navigation_simulator/__init__.py
+++ ocean_navigation_simulator/__init__.py
@@ -1,5 +1,5 @@
 # Make sub-folders visible to the module
-from ocean_navigation_simulator import steering_controllers
+# from ocean_navigation_simulator import steering_controllers
 from ocean_navigation_simulator import planners
 from ocean_navigation_simulator import utils
 from ocean_navigation_simulator import evaluation
@@ -11,4 +11,5 @@ from ocean_navigation_simulator.problem import Problem, WaypointTrackingProblem
 # specify version (for pip installation)
 __version__ = "0.3.0"
 __all__ = ("OceanNavSimulator", "Problem", "WaypointTrackingProblem",
-           "steering_controllers", "utils", "planners", "evaluation")
+           # "steering_controllers",
+           "utils", "planners", "evaluation")
