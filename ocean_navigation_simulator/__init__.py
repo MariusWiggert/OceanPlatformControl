d97519ecf15c73b55ba7db84366db488252c2552
MariusWiggert <mariuswiggert@berkeley.edu>
2021-11-08 18:57:54 -0800

    backwards

--- ocean_navigation_simulator/__init__.py
+++ ocean_navigation_simulator/__init__.py
@@ -1,15 +1,17 @@
 # Make sub-folders visible to the module
 from ocean_navigation_simulator import steering_controllers
-from ocean_navigation_simulator import planners
-from ocean_navigation_simulator import utils
-from ocean_navigation_simulator import evaluation
+# from ocean_navigation_simulator import planners
+# from ocean_navigation_simulator import utils
+# from ocean_navigation_simulator import evaluation
 
-# Make certain classes directly accessible
-from ocean_navigation_simulator.simulator import OceanNavSimulator
-from ocean_navigation_simulator.problem import Problem, WaypointTrackingProblem
+# # Make certain classes directly accessible
+# from ocean_navigation_simulator.simulator import OceanNavSimulator
+# from ocean_navigation_simulator.problem import Problem, WaypointTrackingProblem
 
 # specify version (for pip installation)
 __version__ = "0.3.0"
-__all__ = ("OceanNavSimulator", "Problem", "WaypointTrackingProblem",
-           "steering_controllers",
-           "utils", "planners", "evaluation")
+__all__ = (
+    # "OceanNavSimulator", "Problem", "WaypointTrackingProblem",
+    "steering_controllers",
+    # "utils", "planners", "evaluation"
+)
