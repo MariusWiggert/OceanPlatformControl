efc1f0e6ceefba81628cc3ddd6c671e854b0c1cb
Conor Martin <con-mart@berkeley.edu>
2022-04-05 13:26:14 -0700

    Implemented controller interface and straight_line_controller.py, added simulator data class, units file, and new Problem class

---
+++ ocean_navigation_simulator/controllers/controller.py
@@ -0,0 +1,19 @@
+import abc
+import numpy as np
+from ocean_navigation_simulator.controllers import simulator_data
+
+
+class Controller(abc.ABC):
+    """
+    Interface for controllers.
+    """
+
+    @abc.abstractmethod
+    def get_action(self, observation: simulator_data.SimulatorObservation) -> np.ndarray:
+        """
+        Given an observation, outputs the controller's next action
+        Args:
+          observation: observed state from simulator or other source (i.e. observer, other controller)
+        Returns:
+          Controller's next action as a numpy array.
+        """
---
+++ ocean_navigation_simulator/controllers/problem.py
@@ -0,0 +1,11 @@
+class Problem:
+    """
+    A path planning problem for a Planner to solve.
+
+    Attributes:
+        start_state:
+        end_region:
+        platform_config_dict:
+            A dict specifying the platform parameters
+            TODO: make into class?
+    """
