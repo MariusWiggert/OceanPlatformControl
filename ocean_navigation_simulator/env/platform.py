2d2115ce4cf2683756be54aa39598d83ea23e097
Jerome Jeannin <jeromemjeannin@gmail.com>
2022-04-08 19:18:02 +0200

    battery check and thrust cap with casadi

--- ocean_navigation_simulator/env/platform.py
+++ ocean_navigation_simulator/env/platform.py
@@ -24,36 +24,6 @@ from ocean_navigation_simulator import utils
 
 from ocean_navigation_simulator.utils import plotting_utils, simulation_utils, solar_rad
 
-
-@dataclasses.dataclass
-class PlatformConfig(object):
-    """A dataclass containing constants relevant to the platform state.
-
-      Attributes:
-        date_time: The current time.
-        time_elapsed: The time elapsed in simulation from the time the object was
-            initialized.
-
-        lon:
-        lat:
-
-        solar_charging: The amount of power entering the system via solar panels.
-        power_load: The amount of power being used by the system.
-        battery_charge: The amount of energy stored on the batteries.
-
-        seaweed_mass: The amount of air being pumped by the altitude control
-            system.
-
-        last_command: The previous command executed by the balloon.
-      """
-    battery_cap: units.Energy()
-    u_max: units.Velocity
-    motor_efficiency: float
-
-
-#platform_config_dict = {'battery_cap': 400.0, 'u_max': 0.1, 'motor_efficiency': 1.0,
-#                        'solar_panel_size': 0.5, 'solar_efficiency': 0.2, 'drag_factor': 675}
-
 @dataclasses.dataclass
 class PlatformState(object):
     """A dataclass containing variables relevant to the platform state.
@@ -87,6 +57,36 @@ class PlatformState(object):
 
     seaweed_mass: units.Mass = units.Mass(kg=0)
 
+
+@dataclasses.dataclass
+class PlatformSpecs(object):
+    """A dataclass containing constants relevant to the platform state.
+
+      Attributes:
+        date_time: The current time.
+        time_elapsed: The time elapsed in simulation from the time the object was
+            initialized.
+
+        lon:
+        lat:
+
+        solar_charging: The amount of power entering the system via solar panels.
+        power_load: The amount of power being used by the system.
+        battery_charge: The amount of energy stored on the batteries.
+
+        seaweed_mass: The amount of air being pumped by the altitude control
+            system.
+
+        last_command: The previous command executed by the balloon.
+      """
+    battery_cap: units.Energy(watt_hours=0)
+    u_max: units.Velocity(meters_per_second=0)
+    motor_efficiency: float
+
+
+#platform_config_dict = {'battery_cap': 400.0, 'u_max': 0.1, 'motor_efficiency': 1.0,
+#                        'solar_panel_size': 0.5, 'solar_efficiency': 0.2, 'drag_factor': 675}
+
 class Platform:
     """A simulation of a seaweed platform.
 
@@ -94,20 +94,14 @@ class Platform:
     (simulate_step) for simulating a seaweed platform.
     """
 
-    def __init__(self, state: PlatformState, source: OceanCurrentSource):
+    def __init__(self, state: PlatformState, specs: PlatformSpecs, source: OceanCurrentSource):
         self.state = state
-
-        #data_store = utils.simulation_utils.copernicusmarine_datastore('cmems_mod_glo_phy_anfc_merged-uv_PT1H-i', 'mmariuswiggert', 'tamku3-qetroR-guwneq')
-        #DS_currents = xr.open_dataset(data_store)[['uo', 'vo']].isel(depth=0)
-        #self.old_source = {'data_source_type': 'cop_opendap', 'content': DS_currents, 'grid_dict': Problem.derive_grid_dict_from_xarray(DS_currents)}
-
+        self.specs = specs
         self.source = source
 
         self.sim_settings = {
             'deg_around_x_t': 2,
-            'ti': 2,
-            'hours_to_sim_timescale': 3600,
-            't_horizon_sim': 10,
+            'time_around_x_t': 3600 * 24 * 10,
             'conv_m_to_deg': 111120.
         }
         platform_specs = {
@@ -123,80 +117,56 @@ class Platform:
         self.charge_factor =  (platform_specs['solar_panel_size'] * platform_specs['solar_efficiency']) / cap_in_joule
         self.u_max = platform_specs['u_max']
 
-        self.F_x_next = None
-        self.prev_Dt = None
-
     def simulate_step(
         self,
-        action: float,
+        action: tuple[float, float],
         time_delta: dt.timedelta,
-        stride: dt.timedelta,
+        intermediate_steps: int = 1,
     ) -> PlatformState:
         """Steps forward the simulation.
 
-        This moves the balloon's state forward according to the dynamics of motion
-        for a stratospheric balloon.
+        This moves the platforms's state forward according to the dynamics of motion
 
         Args:
-          action: An AltitudeControlCommand for the system to take during this
-            simulation step, i.e., up/down/stay.
-          time_delta: How much time is elapsing during this step. Must be a multiple
-            of stride.
-          stride: The step size for the simulation of the balloon physics.
+          action:
+          time_delta: How much time is elapsing during this step.
+          intermediate_steps:
         """
-        self.sim_settings['dt'] = time_delta.seconds
-        self.cur_state = np.array([self.state.lon.deg, self.state.lat.deg, self.state.battery_charge.watt_hours, self.state.date_time.timestamp()])
+        state_numpy = np.array([self.state.lon.deg, self.state.lat.deg, self.state.battery_charge.watt_hours, self.state.date_time.timestamp()])
+        u_numpy = np.array(action)
 
-        start = time.time()
-        self.check_dynamics_update(time_delta)
-        #print(f'Update Dynamics: {time.time()-start}s')
-        start = time.time()
-        self.run_step()
-        #print(f'Run Step: {time.time()-start}s')
+        for i in range(intermediate_steps):
+            self.update_dynamics(state_numpy)
+            state_numpy = np.array(self.F_x_next(state_numpy, u_numpy, time_delta.seconds / intermediate_steps)).astype('float64').flatten()
 
-        #print(self.cur_state)
-        self.state.lon = units.Distance(deg=self.cur_state[0])
-        self.state.lat = units.Distance(deg=self.cur_state[1])
-        self.state.battery_charge = units.Energy(watt_hours=self.cur_state[2])
-        self.state.date_time = dt.datetime.fromtimestamp(self.cur_state[3])
+        self.state.lon = units.Distance(deg=state_numpy[0])
+        self.state.lat = units.Distance(deg=state_numpy[1])
+        self.state.battery_charge = units.Energy(watt_hours=state_numpy[2])
+        self.state.date_time = dt.datetime.fromtimestamp(state_numpy[3])
 
         return self.state
 
-    def check_dynamics_update(self, time_delta):
-        """ Helper function for main loop to check if we need to load new current data into the dynamics."""
-        if time_delta != self.prev_Dt:
+    def update_dynamics(self, state_numpy) -> ca.Function:
+        if not hasattr(self, 'F_x_next'):
             start = time.time()
-            self.update_dynamics(self.cur_state.flatten())
+            self.F_x_next = self.get_dynamics(state_numpy)
+            print(f'Initialize Dynamics: {time.time()-start}s')
+        elif not (self.lon_low < state_numpy[0] < self.lon_high) or not (self.lat_low < state_numpy[1] < self.lat_high) or not (self.t_low < state_numpy[3] < self.t_high):
+            if not (self.lon_low < state_numpy[0] < self.lon_high):
+                print(f'Updating Dynamics (Lon: {self.lon_low}, {state_numpy[0]}, {self.lon_high})')
+            if not not (self.lat_low < state_numpy[1] < self.lat_high):
+                print(f'Updating Dynamics (Lat: {self.lat_low}, {state_numpy[1]}, {self.lat_high})')
+            if not not (self.t_low < state_numpy[3] < self.t_high):
+                print(f'Updating Dynamics (Time: {self.t_low}, {state_numpy[3]}, {self.t_high})')
+            start = time.time()
+            self.F_x_next = self.get_dynamics(state_numpy)
             print(f'Update Dynamics: {time.time()-start}s')
-        else:
-            # check based on space and time of the currently loaded current data if new data needs to be loaded
-            if not (self.lon_low < self.cur_state[0] < self.lon_high) or not (self.lat_low < self.cur_state[1] < self.lat_high) or not (self.t_low < self.cur_state[3] < self.t_high):
-                print()
-                print("Updating simulator dynamics with new current data.")
-                if not (self.lon_low < self.cur_state[0] < self.lon_high):
-                    print('Lon:', self.lon_low, self.cur_state[0], self.lon_high)
-                if not not (self.lat_low < self.cur_state[1] < self.lat_high):
-                    print('Lat:', self.lat_low, self.cur_state[1], self.lat_high)
-                if not not (self.t_low < self.cur_state[3] < self.t_high):
-                    print('Time:', self.t_low, self.cur_state[3], self.t_high)
-                print()
-                start = time.time()
-                self.update_dynamics(self.cur_state.flatten())
-                print(f'Update Dynamics Inner: {time.time()-start}s')
 
-        self.prev_Dt = time_delta
-            
-    def update_dynamics(self, x_t):
-        """Update symbolic dynamics function for the simulation by sub-setting the relevant set of current data.
-        Specifically, the self.F_x_next symbolic function and the self.grid_dict
-        Args:
-            x_t: current location of agent
-        Output:
-            F_x_next          cassadi function with (x,u)->(x_next)
-        """
-        lon_interval = [x_t[0] - self.sim_settings["deg_around_x_t"], x_t[0] + self.sim_settings["deg_around_x_t"]]
-        lat_interval = [x_t[1] - self.sim_settings["deg_around_x_t"], x_t[1] + self.sim_settings["deg_around_x_t"]]
-        t_interval = [dt.datetime.fromtimestamp(x_t[3] - 3600, tz=dt.timezone.utc), dt.datetime.fromtimestamp(x_t[3] + 3600 * 24 * 10, tz=dt.timezone.utc)]
+    def get_dynamics(self, state_numpy):
+        ##### Intervals #####
+        lon_interval = [state_numpy[0] - self.sim_settings["deg_around_x_t"], state_numpy[0] + self.sim_settings["deg_around_x_t"]]
+        lat_interval = [state_numpy[1] - self.sim_settings["deg_around_x_t"], state_numpy[1] + self.sim_settings["deg_around_x_t"]]
+        t_interval = [dt.datetime.fromtimestamp(state_numpy[3] - 3600, tz=dt.timezone.utc), dt.datetime.fromtimestamp(state_numpy[3] + self.sim_settings['time_around_x_t'], tz=dt.timezone.utc)]
 
         ##### Get Data #####
         start = time.time()
@@ -209,8 +179,8 @@ class Platform:
         self.lon_low, self.lon_high = self.grid[2].min(), self.grid[2].max()
         self.lat_low, self.lat_high = self.grid[1].min(), self.grid[1].max()
         self.t_low, self.t_high = self.grid[0].min(), self.grid[0].max()
-        print(f'Get Data I: {time.time()-start}s')
-
+        print()
+        print(f'Get Data: {time.time()-start}s')
 
         ##### Interpolation #####
         start = time.time()
@@ -220,61 +190,26 @@ class Platform:
 
         ##### Equations #####
         start = time.time()
-        sym_lon = ca.MX.sym('x1')  # lon
-        sym_lat = ca.MX.sym('x2')  # lat
-        sym_battery = ca.MX.sym('x3')  # battery
-        sym_time = ca.MX.sym('t')  # time
-        u_sim_1 = ca.MX.sym('u_1')  # thrust magnitude in [0,1]
-        u_sim_2 = ca.MX.sym('u_2')  # header in radians
+        sym_lon     = ca.MX.sym('x1')
+        sym_lat     = ca.MX.sym('x2')
+        sym_battery = ca.MX.sym('x3')
+        sym_time    = ca.MX.sym('t')
+        sym_dt      = ca.MX.sym('dt')
+        sym_u_thrust= ca.MX.sym('u_1')
+        sym_u_angle = ca.MX.sym('u_2')
 
-        x_sym = ca.vertcat(sym_lon, sym_lat, sym_battery, sym_time)
-        u_sym = ca.vertcat(u_sim_1, u_sim_2)
+        sym_charge = self.charge_factor * solar_rad(sym_time, sym_lat, sym_lon)
+        sym_u_thrust_capped = ca.fmin(sym_u_thrust, ((sym_battery + sym_charge / sym_dt) / (self.energy_coeff * self.specs.u_max.mps ** 3)) ** (1. / 3))
 
-        lon_dot = (ca.cos(u_sim_2) * u_sim_1 * self.u_max + u_curr_func(ca.vertcat(sym_time, sym_lat, sym_lon))) / self.sim_settings['conv_m_to_deg']
-        lat_dot = (ca.sin(u_sim_2) * u_sim_1 * self.u_max + v_curr_func(ca.vertcat(sym_time, sym_lat, sym_lon))) / self.sim_settings['conv_m_to_deg']
-        battery_dot = self.charge_factor * solar_rad(sym_time, sym_lat, sym_lon) - self.energy_coeff * (self.u_max * u_sim_1) ** 3
-        time_dot = 1
+        sym_lon_next = sym_lon + sym_dt * (ca.cos(sym_u_angle) * sym_u_thrust_capped * self.specs.u_max.mps + u_curr_func(ca.vertcat(sym_time, sym_lat, sym_lon))) / self.sim_settings['conv_m_to_deg']
+        sym_lat_next = sym_lat + sym_dt * (ca.sin(sym_u_angle) * sym_u_thrust_capped * self.specs.u_max.mps + v_curr_func(ca.vertcat(sym_time, sym_lat, sym_lon))) / self.sim_settings['conv_m_to_deg']
+        sym_battery_next = ca.fmin(1, ca.fmax(0, sym_battery + sym_dt * (sym_charge - self.energy_coeff * (self.specs.u_max.mps * sym_u_thrust_capped) ** 3)))
+        sym_time_next = sym_time + sym_dt
 
-        self.F_x_next = ca.Function(
+        F_next = ca.Function(
             'F_x_next',
-            [x_sym, u_sym],
-            [x_sym + self.sim_settings['dt'] * ca.vertcat(lon_dot, lat_dot, battery_dot, time_dot)],
+            [ca.vertcat(sym_lon, sym_lat, sym_battery, sym_time), ca.vertcat(sym_u_thrust, sym_u_angle), sym_dt],
+            [ca.vertcat(sym_lon_next, sym_lat_next, sym_battery_next, sym_time_next)],
         )
-        print(f'Equations: {time.time()-start}s')
-
-    def run_step(self):
-        """Run the simulator for one dt step"""
-
-        #u = self.thrust_check(self.steering_controller.get_next_action(self.cur_state, self.trajectory))
-        u = np.array([1,3.14/2])
-        self.cur_state = np.array(self.F_x_next(self.cur_state, u)).astype('float64').flatten()
-
-
-    def thrust_check(self, u_planner):
-        """If the thrust would use more energy than available adjust accordingly."""
-
-        if np.isnan(u_planner).sum() > 0 :
-            raise ValueError("Planner action contains Nan, specifically u_planner=", u_planner)
-
-        charge = self.charge_factor * solar_rad(self.cur_state[3], self.cur_state[1], self.cur_state[0])
-
-        delta_charge = charge -  self.energy_coeff * (self.u_max * u_planner[0]) ** 3
-
-        next_charge = self.cur_state[2] + delta_charge * self.sim_settings['dt']
-
-        # if smaller than 0.: change the thrust accordingly
-        if next_charge < 0.:
-            energy_available = self.cur_state[2]
-            u_planner[0] = ((charge - energy_available / dt) / (self.energy_coeff * self.u_max ** 3)) ** (1. / 3)
-            return u_planner
-        else:
-            return u_planner
-
-
-    @staticmethod
-    def battery_check(cur_state):
-        """Prevents battery level to go above 1."""
-        if cur_state[2] > 1.:
-            cur_state[2] = 1.
-        return cur_state
-
+        print(f'Set Equations: {time.time()-start}s')
+        return F_next
--- scripts/experiments/test_new_simulation.py
+++ scripts/experiments/test_new_simulation.py
@@ -7,7 +7,7 @@ import time
 
 from ocean_navigation_simulator.data_sources.OceanCurrentSource.OceanCurrentSource import ForecastFileSource, HindcastFileSource, HindcastOpendapSource
 from ocean_navigation_simulator.data_sources.OceanCurrentFields import OceanCurrentField
-from ocean_navigation_simulator.env.platform import PlatformState, Platform
+from ocean_navigation_simulator.env.platform import Platform, PlatformState, PlatformSpecs
 from ocean_navigation_simulator.utils import plotting_utils, simulation_utils, solar_rad, units
 from ocean_navigation_simulator.problem import Problem
 from ocean_navigation_simulator import utils
@@ -15,7 +15,7 @@ from ocean_navigation_simulator import utils
 script_start = time.time()
 
 t_0 = datetime.datetime(year=2021, month=11, day=23, hour=12, minute=10, second=10, tzinfo=datetime.timezone.utc)
-horizon = datetime.timedelta(days=10)
+horizon = datetime.timedelta(days=5)
 stride = datetime.timedelta(minutes=1)
 steps = int(horizon / stride)
 
@@ -33,12 +33,25 @@ source = HindcastOpendapSource(
 )
 print(f'Init Source: {time.time()-start}s')
 
-platform = Platform(state=PlatformState(date_time=t_0, lon=units.Distance(deg=-81.5), lat=units.Distance(deg=23.5)), source=source)
-trajectory = np.zeros((steps, 2))
+##### Initialize Platform #####
+start = time.time()
+init_state = PlatformState(
+    date_time=t_0,
+    lon=units.Distance(deg=-81.5),
+    lat=units.Distance(deg=23.5)
+)
+platform_specs = PlatformSpecs(
+    battery_cap=units.Energy(watt_hours=1000),
+    u_max=units.Velocity(mps=0.1),
+    motor_efficiency=1.0
+)
+platform = Platform(state=init_state, source=source, specs=platform_specs)
+print(f'Init Platform: {time.time()-start}s')
 
+trajectory = np.zeros((steps, 2))
 ##### Run Simulation #####
 for step in tqdm(range(steps)):
-    state = platform.simulate_step(action=0, time_delta=stride,stride=stride)
+    state = platform.simulate_step(action=[1,3.14/2], time_delta=stride)
     trajectory[step] = np.array([state.lon.deg, state.lat.deg])
 
 ##### Plotting using old Utils #####
