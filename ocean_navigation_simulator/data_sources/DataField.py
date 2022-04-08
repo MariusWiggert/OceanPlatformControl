136397601a64bb3babd46172892d82df06295a5d
MariusWiggert <mariuswiggert@berkeley.edu>
2022-04-08 16:11:43 -0700

    slight changes to OceanCurrents & DataField

--- ocean_navigation_simulator/data_sources/DataField.py
+++ ocean_navigation_simulator/data_sources/DataField.py
@@ -1,20 +1,21 @@
 import abc
 import datetime
-from typing import List, NamedTuple, Sequence
+from typing import List, NamedTuple, Sequence, Callable, Optional
+import numpy as np
 
 # import gin # We don't use gin because it doesn't work well with the C3 Data Types. Hence, we use settings_dicts.
 import xarray as xr
 
 
 class DataField(abc.ABC):
-  """Abstract class for lookups in an DataField.
+    """Abstract class for lookups in an DataField.
   Both point-based lookup (for simulation) and spatio-temporal interval lookup (for planning)
   of both ground_truth and forecasted Data (e.g. Ocean currents, solar radiation, seaweed growth)
   """
 
-  @abc.abstractmethod
-  def get_forecast(self, point: List[float], time: datetime.datetime):
-    """Returns forecast at a point in the field.
+    @abc.abstractmethod
+    def get_forecast(self, point: List[float], time: datetime.datetime):
+        """Returns forecast at a point in the field.
     Args:
       point: Point in the respective used coordinate system (lat, lon for geospherical or unitless for examples)
       time: absolute datetime object
@@ -22,10 +23,10 @@ class DataField(abc.ABC):
       A Field Data for the position in the DataField (Vector or other).
     """
 
-  @abc.abstractmethod
-  def get_forecast_area(self, x_interval: List[float], y_interval: List[float], t_interval: List[datetime.datetime],
-                        spatial_resolution: float, temporal_resolution) -> xr:
-    """A function to receive the forecast for a specific area over a time interval.
+    @abc.abstractmethod
+    def get_forecast_area(self, x_interval: List[float], y_interval: List[float], t_interval: List[datetime.datetime],
+                          spatial_resolution: float, temporal_resolution) -> xr:
+        """A function to receive the forecast for a specific area over a time interval.
     Args:
       x_interval: List of the lower and upper x area in the respective coordinate units [x_lower, x_upper]
       y_interval: List of the lower and upper y area in the respective coordinate units [y_lower, y_upper]
@@ -36,9 +37,9 @@ class DataField(abc.ABC):
       data_array                    in xarray format that contains the grid and the values
     """
 
-  @abc.abstractmethod
-  def get_ground_truth(self, point: List[float], time: datetime.datetime):
-    """Returns true data at a point in the field.
+    @abc.abstractmethod
+    def get_ground_truth(self, point: List[float], time: datetime.datetime):
+        """Returns true data at a point in the field.
     Args:
       point: Point in the respective used coordinate system e.g. [lon, lat] for geospherical or unitless for examples
       time: absolute datetime object
@@ -46,10 +47,11 @@ class DataField(abc.ABC):
       A Field Data for the position in the DataField (Vector or other).
     """
 
-  @abc.abstractmethod
-  def get_ground_truth_area(self, x_interval: List[float], y_interval: List[float], t_interval: List[datetime.datetime],
-                            spatial_resolution: float, temporal_resolution) -> xr:
-    """A function to receive the ground_truth for a specific area over a time interval.
+    @abc.abstractmethod
+    def get_ground_truth_area(self, x_interval: List[float], y_interval: List[float],
+                              t_interval: List[datetime.datetime],
+                              spatial_resolution: float, temporal_resolution) -> xr:
+        """A function to receive the ground_truth for a specific area over a time interval.
     Args:
       x_interval: List of the lower and upper x area in the respective coordinate units [x_lower, x_upper]
       y_interval: List of the lower and upper y area in the respective coordinate units [y_lower, y_upper]
@@ -59,3 +61,25 @@ class DataField(abc.ABC):
     Returns:
       data_array                    in xarray format that contains the grid and the values
     """
+
+    @staticmethod
+    def interpolate_in_space_and_time(array: xr, spatial_resolution: Optional[float],
+                                      temporal_resolution: Optional[float]) -> xr:
+        """Helper function for temporal and spatial interpolation"""
+        # Run temporal interpolation
+        if temporal_resolution is not None:
+            time_grid = np.arange(start=array['time'][0].data, stop=array['time'][-1].data,
+                                  step=np.timedelta64(temporal_resolution, 's'))
+            array = array.interp(time=time_grid, method='linear')
+
+        # Run spatial interpolation
+        if spatial_resolution is not None:
+            lat_grid = np.arange(start=array['lat'][0].data, stop=array['lat'][-1].data,
+                                 step=spatial_resolution)
+            lon_grid = np.arange(start=array['lon'][0].data, stop=array['lon'][-1].data,
+                                 step=spatial_resolution)
+            array = array.interp(
+                lon=lon_grid,
+                lat=lat_grid, method='linear')
+
+        return array
--- ocean_navigation_simulator/data_sources/OceanCurrentFields.py
+++ ocean_navigation_simulator/data_sources/OceanCurrentFields.py
@@ -52,9 +52,6 @@ class OceanCurrentField(DataField):
 
         return current_array
 
-        # # again fill na with 0
-        # raw_current_array = raw_current_array.fillna(0)
-
     def get_ground_truth(self, point: List[float], time: datetime.datetime) -> OceanCurrentVector:
         """Returns true data at a point in the field.
         Args:
@@ -66,7 +63,7 @@ class OceanCurrentField(DataField):
 
     def get_ground_truth_area(self, x_interval: List[float], y_interval: List[float],
                               t_interval: List[datetime.datetime],
-                              spatial_resolution: float, temporal_resolution) -> xr:
+                              spatial_resolution: Optional[float] = None, temporal_resolution: Optional[float] = None) -> xr:
         """A function to receive the ground_truth for a specific area over a time interval.
         Args:
           x_interval: List of the lower and upper x area in the respective coordinate units [x_lower, x_upper]
@@ -78,25 +75,15 @@ class OceanCurrentField(DataField):
           data_array                    in xarray format that contains the grid and the values
         """
 
-    def interpolate_in_space_and_time(self, array: xr, spatial_resolution: Optional[float],
-                                      temporal_resolution: Optional[float]) -> xr:
-        """Helper function for interpolation"""
-        # Run temporal interpolation
-        if temporal_resolution is not None:
-            time_grid = np.arange(start=array['time'][0].data, stop=array['time'][-1].data,
-                                  step=np.timedelta64(temporal_resolution, 's'))
-            array = array.interp(time=time_grid, method='linear')
+        # Step 1: get the raw sub-setted xarray
+        current_array = self.hindcast_data_source.get_currents_over_area(x_interval, y_interval, t_interval)
 
-        # Run spatial interpolation
-        if spatial_resolution is not None:
-            lat_grid = np.arange(start=array['lat'][0].data, stop=array['lat'][-1].data,
-                                 step=spatial_resolution)
-            lon_grid = np.arange(start=array['lon'][0].data, stop=array['lon'][-1].data,
-                                 step=spatial_resolution)
-            array = array.interp(
-                lon=lon_grid,
-                lat=lat_grid, method='linear')
+        # Step 2: perform interpolation to a specific resolution if requested
+        if spatial_resolution is not None or temporal_resolution is not None:
+            current_array = self.interpolate_in_space_and_time(current_array, spatial_resolution, temporal_resolution)
 
-        return array
+        return current_array
+
+
 
 
