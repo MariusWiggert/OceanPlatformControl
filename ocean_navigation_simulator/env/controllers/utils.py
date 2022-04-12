0c8e34092c5c194ecc2292cb0430afff269e72eb
Conor Martin <con-mart@berkeley.edu>
2022-04-12 12:28:35 -0700

    complete naive controller, update platform_env.py, problem.py, problem_factory.py

--- ocean_navigation_simulator/env/controllers/utils.py
+++ ocean_navigation_simulator/env/controllers/utils.py
@@ -1,18 +1,19 @@
+from typing import Tuple
+
 import numpy as np
 
 
-def transform_u_dir_to_u(u_dir) -> np.ndarray:
+def transform_u_dir_to_u(u_dir) -> Tuple[float, float]:
     """ Transforms the given u and v velocities to the corresponding heading and thrust.
 
     Args:
         u_dir:
-            An nested array containing the u and velocities. More rigorously:
+            An nested array containing the u and v velocities. More rigorously:
             array([[u velocity (longitudinal)], [v velocity (latitudinal)]])
 
     Returns:
-        An array containing the thrust and heading, i.e. array([thrust], [heading]).
-        Heading is in radians.
+        Thrust in [] and heading in radians
     """
     thrust = np.sqrt(u_dir[0] ** 2 + u_dir[1] ** 2)  # Calculating thrust from distance formula on input u
     heading = np.arctan2(u_dir[1], u_dir[0])  # Finds heading angle from input u
-    return np.array([thrust, heading])
+    return thrust, heading
