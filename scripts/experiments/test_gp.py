ba5e3c3c35f20dbcb766dfc0a0018968511ffc66
killian2k <killian.kaempf@gmail.com>
2022-04-20 14:05:20 -0700

    Use the correct forecast and hindcast

--- scripts/experiments/test_gp.py
+++ scripts/experiments/test_gp.py
@@ -35,10 +35,15 @@ hindcast_file_config_dict = {'source': 'hindcast_files',
                     'folder': "data/single_day_hindcasts/"}, #"data/hindcast_test/"
                 }
 
+#%% sim_cache dict
+sim_cache = {}
+
 #%% Step 2: Instantiate the field
 # current_field = OceanCurrentField.OceanCurrentField(hindcast_source_dict = source_dict | hindcast_file_config_dict,
 #                                                     forecast_source_dict= source_dict | forecast_file_config_dict)
-current_field = OceanCurrentField.OceanCurrentField(hindcast_source_dict = source_dict | forecast_file_config_dict, forecast_source_dict=source_dict|forecast_file_config_dict)
+current_field = OceanCurrentField.OceanCurrentField(sim_cache,
+                                                    hindcast_source_dict=source_dict|hindcast_file_config_dict,
+                                                    forecast_source_dict=source_dict|forecast_file_config_dict)
 # Test settings to use it
 t_0 = datetime.datetime.now() + datetime.timedelta(hours=10)
 
@@ -46,7 +51,9 @@ t_0 = datetime.datetime.now() + datetime.timedelta(hours=10)
 # long: -95.053711  -84.111328
 # lat:  23.497506    28.124313
 
-t_interval = [datetime.datetime(2021,11,22), datetime.datetime(2021,12,26)]
+#t_interval = [datetime.datetime(2021,11,28), datetime.datetime(2021,12,12)]
+t_0 = datetime.datetime(2021, 11, 26, 23, 30, tzinfo=datetime.timezone.utc)
+t_interval = [t_0, t_0 + datetime.timedelta(days=25)]
 x_interval=[-95.053711, -84.111328]
 y_interval=[23.497506, 28.124313]
 
@@ -55,10 +62,20 @@ x_T = [-80, 24.2]
 
 
 #%%
-vec_point = current_field.get_forecast_area(x_interval, y_interval, t_interval)
-print(vec_point)
+vec_point_forecast = current_field.get_forecast_area(x_interval, y_interval, t_interval)
+print(vec_point_forecast)
+#%%
+vec_point_hindcast = current_field.get_ground_truth_area(x_interval, y_interval, t_interval)
+#%% Convert the data into arrays for training and testing
+array_forecast = vec_point_forecast.to_array().transpose("time","lat","lon","variable")
+array_hindcast = vec_point_forecast.to_array().transpose("time","lat","lon","variable")
+ratio,num_samples = .8, array_forecast.shape[0]
+split = int(ratio * num_samples)
+X_tr, y_tr = array_forecast[:split,:,:],array_hindcast[:split,:,:]
+X_te, y_te = array_forecast[split:,:,:],array_hindcast[split:,:,:]
+print(X_tr.shape,X_te.shape,y_tr.shape,y_te.shape)
+
 #%% New part
-from scripts.experiments.class_gp import OceanCurrentGP
 model = OceanCurrentGP(current_field)
 x,y = units.Distance(m=0.0), units.Distance(m=0.0)
 t_0 = datetime.datetime.fromtimestamp(10, datetime.timezone.utc)
@@ -66,8 +83,17 @@ t_0 = datetime.datetime(2022, 4, 4, 23, 30, tzinfo=datetime.timezone.utc)
 ocean_current_vector = OceanCurrentVector(1.0, 1.0)
 
 #%%
-vec_point = current_field.get_forecast(point=[x.m, y.m], time=t_0)
-print('forecast: ', vec_point)
+for i,time in enumerate(y_tr["time"]):
+    #Shape elem: lat
+    for j,lat in enumerate(y_tr["lat"]):
+        for k,long in enumerate(y_tr["lon"]):
+            print("toto")
+            print(lat,long,OceanCurrentVector(*y_tr[i,j,k].to_numpy()))
+            print("titi")
+            model.observe(lat,long,i,OceanCurrentVector(*y_tr[i,j,k].to_numpy()))
+#%%
+vec_point_forecast = current_field.get_forecast(point=[x.m, y.m], time=t_0)
+print('forecast: ', vec_point_forecast)
 pre_measurement = model.query(x, y, t_0)
 model.observe(x, y, t_0, ocean_current_vector)
 post_measurement = model.query(x, y, t_0)
