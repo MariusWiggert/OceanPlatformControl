ed4cebd4a28482bbd74cd5ce58d6e8a45d11b2e1
Jerome Jeannin <jeromemjeannin@gmail.com>
2022-04-28 15:40:43 -0700

    test script for naive controller

--- scripts/experiments/test_double_gyre_problem_factory.py
+++ scripts/experiments/test_double_gyre_problem_factory.py
@@ -1,18 +1,39 @@
+from tqdm import tqdm
+
 from ocean_navigation_simulator.env.ArenaFactory import ArenaFactory
 from ocean_navigation_simulator.env.DoubleGyreProblemFactory import DoubleGyreProblemFactory
 
+import time
+
+from ocean_navigation_simulator.env.controllers.NaiveToTarget import NaiveToTargetController
+
+start = time.time()
 
 factory = DoubleGyreProblemFactory()
 
-problem = factory.next_problem()
+for j in range(10):
+    problem = factory.next_problem()
+    arena = ArenaFactory.create(scenario_name='double_gyre')
+    controller = NaiveToTargetController(problem=problem)
+    observation = arena.reset(problem.start_state)
 
-print(problem.start_state, problem.end_region, problem.radius)
+    for i in tqdm(range(5000)):
+        action = controller.get_action(observation)
+        observation = arena.step(action)
+        if problem.is_done(observation.platform_state):
+            print('Problem solved!')
+            break
 
+    ax = arena.plot_spatial(
+        ax=None if j==0 else ax,
+        background='currents' if j==0 else None,
+        problem=problem,
+        show_control=False,
+        show_trajectory=True,
+        margin=2,
+    )
 
-arena, platform_state, observation, end_region = ArenaFactory.create(scenario_name='double_gyre')
+ax.get_figure().show()
 
-arena.plot_spatial(
-    problem=problem,
-    margin=2,
-).get_figure().show()
+print("Total Script Time: ", time.time() - start)
 
