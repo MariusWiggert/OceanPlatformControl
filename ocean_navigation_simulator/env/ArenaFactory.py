2510678fd44eac40fbe2785ea23ec1a6b2960e38
Jerome Jeannin <jeromemjeannin@gmail.com>
2022-05-23 16:49:11 -0700

    debug paths

--- ocean_navigation_simulator/env/ArenaFactory.py
+++ ocean_navigation_simulator/env/ArenaFactory.py
@@ -9,7 +9,7 @@ from ocean_navigation_simulator.env.Arena import Arena
 
 class ArenaFactory:
     @staticmethod
-    def create(scenario_name: string) -> Arena:
+    def create(scenario_name: string):# -> Arena:
         print('test')
         print(__file__)
         print(Path(__file__).parent.resolve())
@@ -19,15 +19,15 @@ class ArenaFactory:
         print(Path().resolve())
         print(abspath(os.getcwd()))
 
-        with open(f'{dirname(abspath(__file__))}/scenarios/{scenario_name}.yaml') as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
-
-        return Arena(
-            sim_cache_dict=config['sim_cache_dict'],
-            platform_dict=config['platform_dict'],
-            ocean_dict=config['ocean_dict'],
-            use_geographic_coordinate_system=config['use_geographic_coordinate_system'],
-            solar_dict=config['solar_dict'],
-            seaweed_dict=config['seaweed_dict'],
-            spatial_boundary=config['spatial_boundary']
-        )
+        # with open(f'{dirname(abspath(__file__))}/scenarios/{scenario_name}.yaml') as f:
+        #     config = yaml.load(f, Loader=yaml.FullLoader)
+        #
+        # return Arena(
+        #     sim_cache_dict=config['sim_cache_dict'],
+        #     platform_dict=config['platform_dict'],
+        #     ocean_dict=config['ocean_dict'],
+        #     use_geographic_coordinate_system=config['use_geographic_coordinate_system'],
+        #     solar_dict=config['solar_dict'],
+        #     seaweed_dict=config['seaweed_dict'],
+        #     spatial_boundary=config['spatial_boundary']
+        # )
