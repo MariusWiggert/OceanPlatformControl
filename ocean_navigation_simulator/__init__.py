f2af906c9a41b3ab8734eb197a374d97000b4c39
Jerome Jeannin <jeromemjeannin@gmail.com>
2022-05-23 01:29:17 -0700

    updated init

--- ocean_navigation_simulator/__init__.py
+++ ocean_navigation_simulator/__init__.py
@@ -1,1 +1,17 @@
-__version__ = "Jerome_value_function_learning"
+# Make sub-folders visible to the module
+from ocean_navigation_simulator import steering_controllers
+from ocean_navigation_simulator import planners
+from ocean_navigation_simulator import utils
+from ocean_navigation_simulator import evaluation
+from ocean_navigation_simulator import env
+
+
+# specify version (for pip installation)
+__version__ = "0.0.1"
+__all__ = (
+    "steering_controllers",
+    "planners",
+    "evaluation",
+    "env",
+    "utils",
+)
