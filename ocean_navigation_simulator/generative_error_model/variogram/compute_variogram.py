447a21157277762006910882f64be28a06147c0d
Jonas Dieker <jonasdieker05@gmail.com>
2022-08-10 09:58:55 -0700

    fixed cmd line arg input + added more informative cmd line output

--- ocean_navigation_simulator/generative_error_model/variogram/compute_variogram.py
+++ ocean_navigation_simulator/generative_error_model/variogram/compute_variogram.py
@@ -3,44 +3,56 @@ from ocean_navigation_simulator.generative_error_model.utils import timer
 from Variogram import Variogram
 
 import argparse
-from typing import Tuple, List
 import numpy as np
 import os
 
 
 def parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument("bin_res", nargs="+", type=List[float], help="defines the ranges of bins [lon, lat, time]")
-    parser.add_argument("--total_bin_res", default=22, type=int, help="only used for 1D variogram")
+    parser.add_argument("bin_res", nargs=3, action="append", type=float, help="defines the ranges of bins [lon, lat, time]")
+    parser.add_argument("--num_workers", default=2, type=int, help="for multiprocessing")
     parser.add_argument("--chunk_size", default=1e6, type=float, help="what size chunk the computation is performed on")
     parser.add_argument("--detrended", default=True, type=bool, help="if the detrended data should be used")
     parser.add_argument("--cross_buoy_pairs_only", default=False, type=bool, help="read name m8")
+    parser.add_argument("--dataset_size", default="large", type=str, help="{large -> month, small -> single file}")
     return parser
 
 
 @timer
 def main():
-    # args =  parse().parse_args()
-    dataset_name = DatasetName.AREA1
-    # data = load_dataset(dataset_name) # 300,000 pts
-    data = load_single_file(dataset_name, file_idx=0)  # 8,000 pts
+    args =  parse().parse_args()
 
-    # Variogram calculation
+    # load data
+    dataset_name = DatasetName.AREA1
+    if args.dataset_size == "large":
+        data = load_dataset(dataset_name) # 300,000 pts
+    else:
+        data = load_single_file(dataset_name, file_idx=0)  # 8,000 pts
+
+    # initialize object + detrend
     V = Variogram(data)
     V.detrend(detrend_var="lat", num_bins=1)
 
     # define hyper-params
-    bin_res = (0.05,0.05,1)
-    detrended = True
-    cross_buoy_pairs_only = False
-    print("Begin computing variogram.")
+    bin_res = args.bin_res[0]
+    num_workers = args.num_workers
+    detrended = args.detrended
+    cross_buoy_pairs_only = args.cross_buoy_pairs_only
+    print("\nBegin computing variogram.")
+    print(f"""PARAMS:
+        bin_res={bin_res},d
+        num_workers={args.num_workers},
+        detrended={args.detrended},
+        cross_buoy_pairs_only={args.cross_buoy_pairs_only}
+        dataset_size={args.dataset_size}\n""")
+
 
     # compute variogram
-    bins, bins_count = V.build_variogram_gen(bin_res, 2, chunk_size=int(1e6),\
+    bins, bins_count = V.build_variogram_gen(bin_res, num_workers, chunk_size=int(1e6),\
         detrended=detrended, cross_buoy_pairs_only=cross_buoy_pairs_only)
 
     # save to numpy array
-    file_name = f"small_3d_variogram_{dataset_name.name}_{bin_res[0]}_{bin_res[1]}_{bin_res[2]}_{detrended}_{cross_buoy_pairs_only}.npy"
+    file_name = f"3d_variogram_{dataset_name.name}_{bin_res[0]}_{bin_res[1]}_{bin_res[2]}_{detrended}_{cross_buoy_pairs_only}.npy"
     file_path = os.path.join("/home/jonas/Documents/Thesis/OceanPlatformControl/data/drifter_data/variogram", file_name)
     data_to_save = {"bins":bins, "bins_count":bins_count, "res": [V.lon_res, V.lat_res, V.t_res]}
     np.save(file_path, data_to_save)
