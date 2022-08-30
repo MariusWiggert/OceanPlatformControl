5703671e95a3d832a95ffc0e23dbcf0932ab1cc5
Jonas Dieker <jonasdieker05@gmail.com>
2022-08-30 11:20:53 -0700

    improved plotting for curve fitting and fixed bug in 1d curve

--- ocean_navigation_simulator/generative_error_model/variogram/VariogramFitting.py
+++ ocean_navigation_simulator/generative_error_model/variogram/VariogramFitting.py
@@ -27,8 +27,8 @@ class VariogramFitting:
             lags.append(self.data[lag_var].to_numpy().reshape(-1, 1))
         self.lags = np.hstack(tuple(lags))
 
-    def fit_model(self, num_of_components, constrain_weighting=False):
-        """Takes the lags and their associated values and fits multiple stacked gaussian models to it.
+    def fit_model(self, num_of_components: int, method: str = "leastsq", constrain_weighting: bool = False, verbose: bool = False):
+        """Uses the lags and their associated values and fits multiple stacked gaussian models to it.
         The output is of size [n,d]:
             n - number of harmonics
             d - lag space dimension
@@ -49,12 +49,20 @@ class VariogramFitting:
         print(f"Type of model: {model_type.__name__}")
 
         params = self.initialise_parameters(constrain_weighting=constrain_weighting)
-        # TODO: try weights
+        # TODO: try weights (need to be same shape as data)
+        weights_1d = list(np.logspace(0.1, 1, self.lags.shape[0]))
+        weights = weights_1d
         # fit model: data points, parameters, lags for the data points (dependent variables)
-        result = self.model.fit(self.data[self.error_var], params, lag_vec=self.lags)
+        result = self.model.fit(self.data[self.error_var],
+                                params=params,
+                                lag_vec=self.lags,
+                                method=method,
+                                weights=None)
 
         self.popt = np.array(list(result.params.valuesdict().values())).reshape(-1, len(self.lag_vars)+1)
         print(f"Parameters:\n {self.popt}")
+        if verbose:
+            print(result.fit_report())
 
     def initialise_parameters(self, constrain_weighting=False):
         """Initializes every parameter for every component of the model.
@@ -71,37 +79,50 @@ class VariogramFitting:
                     params.add(f"{component.prefix}{range_var}", value=np.random.rand() * 1000, max=2000, min=10)
                 weights_list.append(f"{component.prefix}s")
             else:
-                params.add(f"{component.prefix}s", value=np.random.rand(), max=1, min=0)
+                params.add(f"{component.prefix}s", value=1/self.num_components, max=1, min=0)
                 for range_var in self.range_vars:
                     params.add(f"{component.prefix}{range_var}", value=np.random.rand() * 1000, max=2000, min=10)
         return params
 
-    def plot_sliced_variogram(self, var: str):
+    def plot_all_dims(self, save_path: str = None):
+        """Plot the sliced fitted function over all lag variables.
+        """
+        figure, axs = plt.subplots(len(self.lag_vars), figsize=(20, 20))
+        for idx, var in enumerate(self.lag_vars):
+            self._plot_sliced_variogram(var, axs[idx])
+        plt.tight_layout()
+        if save_path:
+            plt.savefig(save_path, pad_inches=0, facecolor="white")
+        return figure
+
+    def _plot_sliced_variogram(self, var: str, ax: plt.axis) -> plt.axis:
         """Plots the fitted function vs the empirical data points.
         """
         if var not in self.lag_vars:
             raise ValueError(f"Your specified var='{var}' is not in {self.lag_vars}!")
-        lag_idx = self.lag_vars.index(var)
-        lags = self.lags[:, lag_idx]
+
         # get data for fitted function line plot
-        function_semivariance = self._get_sliced_fitted_function_data(var, lags)
+        lags, function_semivariance = self._get_sliced_fitted_function_data(var)
         # get data for empirical points scatter plot
         empirical_lags, empirical_semivariance = self._get_sliced_empirical_data(var)
 
         # visualize slice
-        _ = plt.figure(figsize=(20, 12))
-        plt.plot(lags, function_semivariance, label="fitted function")
-        plt.scatter(empirical_lags, empirical_semivariance, label="empirical points")
-        plt.legend()
-        plt.xlim(left=0)
-        plt.ylim([0, 1.5])
-        plt.xlabel(f"{var}")
-        plt.ylabel("semivariance")
+        ax.plot(lags, function_semivariance, label="fitted function", color="orange")
+        ax.scatter(empirical_lags, empirical_semivariance, label="empirical points")
+        ax.legend()
+        ax.set_xlim(left=0)
+        ax.set_ylim([0, 1.5])
+        ax.set_xlabel(f"{var}")
+        ax.set_ylabel("semivariance")
+        return ax
 
-    def _get_sliced_fitted_function_data(self, var: str, lags: List[float]):
+    def _get_sliced_fitted_function_data(self, var: str):
         """Uses the computed parameters to slice the function in the variable specified.
         """
         # TODO: Maybe use self.model.eval() here which is part of lmfit
+        lag_idx = self.lag_vars.index(var)
+        # need to create set of lags because there will be multiples
+        lags = sorted(list(set(self.lags[:, lag_idx])))
         semivariance = []
         for component in (range(self.popt.shape[0])):
             weighting = self.popt[component, 0]
@@ -110,7 +131,7 @@ class VariogramFitting:
                 semivariance = np.array(self._get_values_from_fitted_function(lags, weighting, range_param))
             else:
                 semivariance += np.array(self._get_values_from_fitted_function(lags, weighting, range_param))
-        return semivariance
+        return lags, semivariance
 
     def _get_sliced_empirical_data(self, var: str):
         """Slices the empirical data to get data for the variable specified.
@@ -145,7 +166,7 @@ class VariogramFitting:
 
     def _gaussian_slice(self, lag, s, r):
         """1D version of gaussian function for visualizing slices."""
-        gamma = s*(1-np.exp(-3*(1/r**2)*lag**2))
+        gamma = s*(1 - np.exp(-3 * (1/r**2) * lag**2) + 0.06 * np.exp(-7.07 * (1/r**2) * lag**2))
         return gamma
 
 
