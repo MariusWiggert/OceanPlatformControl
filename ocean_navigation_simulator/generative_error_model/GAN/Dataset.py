d9404b46c368f7080a3c7e0827bfa56fd75f9bb9
Jonas Dieker <jonasdieker05@gmail.com>
2022-09-25 21:30:34 -0700

    pipeline successfully runs now

--- ocean_navigation_simulator/generative_error_model/GAN/Dataset.py
+++ ocean_navigation_simulator/generative_error_model/GAN/Dataset.py
@@ -9,10 +9,12 @@ import torchvision.transforms as transforms
 
 class BuoyForecastError(Dataset):
 
-    def __init__(self, fc_dir, gt_dir, transform=None) -> None:
+    def __init__(self, fc_dir, gt_dir, length=None, transform=None) -> None:
         self.fc_dir = fc_dir
         self.gt_dir = gt_dir
+        self.length = length
         self.transform = transform
+        self.hours_in_file = 24
 
         # get all required file names
         self.fc_file_names = sorted(os.listdir(self.fc_dir))
@@ -22,31 +24,37 @@ class BuoyForecastError(Dataset):
             warn("Datasets do not contain same number of files!")
 
     def __len__(self):
-        return
+        if self.length:
+            return self.length
+        else:
+            return len(self.fc_file_names) * self.hours_in_file
 
     def __getitem__(self, idx):
 
-        file_idx = idx // 227
-        time_step_idx = idx % 227
+        file_idx = idx // self.hours_in_file
+        time_step_idx = idx % self.hours_in_file
 
         self.fc_file_name = self.fc_file_names[file_idx]
         self.error_file_name = self.error_file_names[file_idx]
 
         # get time step for FC
-        fc_file = xr.load_dataset(os.path.join(self.fc_dir, self.fc_file_name)).sel(longitude=slice(-140, -120 - 1/12),
-                                                         latitude=slice(20, 30 - 1/12))
+        fc_file = xr.load_dataset(os.path.join(self.fc_dir, self.fc_file_name)).sel(longitude=slice(-140, -120),
+                                                                                    latitude=slice(20, 30))
         fc_file_u = fc_file["utotal"]
         fc_file_v = fc_file["vtotal"]
         fc_u = fc_file_u.isel(time=time_step_idx).values.squeeze()
-        fc_u = fc_u[:, :, np.newaxis]
+        fc_u = fc_u[np.newaxis, :, :]
         fc_v = fc_file_v.isel(time=time_step_idx).values.squeeze()
-        fc_v = fc_v[:, :, np.newaxis]
-        fc = np.concatenate([fc_u, fc_v], axis=2)
+        fc_v = fc_v[np.newaxis, :, :]
+        fc = np.concatenate([fc_u, fc_v], axis=0)
 
         # get time step for error
         error_file = pd.read_csv(os.path.join(self.gt_dir, self.error_file_name))
         error_file["hour"] = error_file["time"].apply(lambda x: x[:13])
         hours = sorted(set(error_file["hour"].tolist()))
+        # if len(hours) != self.hours_in_file:
+        #     warn("Time axis are not equal between FC and Sparse GT!")
+        # print(len(hours), time_step_idx, idx)
         sparse_error_time_step = error_file[error_file["hour"].values == hours[time_step_idx]]
 
         # get sparse data on FC grid.
@@ -56,11 +64,15 @@ class BuoyForecastError(Dataset):
         lat_idx = np.searchsorted(fc_file["latitude"].values, nearest_grid_points[1])
 
         # write sparse error arrays
-        sparse_error_u = np.zeros_like(fc_u)
-        sparse_error_u[lat_idx, lon_idx] = sparse_error_time_step["u_error"].values.reshape(-1, 1)
-        sparse_error_v = np.zeros_like(fc_v)
-        sparse_error_v[lat_idx, lon_idx] = sparse_error_time_step["v_error"].values.reshape(-1, 1)
-        sparse_error = np.concatenate([sparse_error_u, sparse_error_v], axis=2)
+        sparse_error_u = np.zeros_like(fc_u).squeeze()
+        sparse_error_u[lat_idx, lon_idx] = sparse_error_time_step["u_error"].values.squeeze()
+        sparse_error_v = np.zeros_like(fc_v).squeeze()
+        sparse_error_v[lat_idx, lon_idx] = sparse_error_time_step["v_error"].values.squeeze()
+
+        sparse_error_u = sparse_error_u[np.newaxis, :, :]
+        sparse_error_v = sparse_error_v[np.newaxis, :, :]
+
+        sparse_error = np.concatenate([sparse_error_u, sparse_error_v], axis=0)
 
         if self.transform:
             # apply transformations
@@ -77,8 +89,14 @@ def test():
     fc_dir = os.path.join(data_dir, "data/drifter_data/forecasts/area1")
     gt_dir = os.path.join(data_dir, "data/drifter_data/dataset_forecast_error/area1")
     dataset = BuoyForecastError(fc_dir, gt_dir)
-    dataset_item = dataset.__getitem__(0)
+    idx = np.random.randint(1000)
+    dataset_item = dataset.__getitem__(idx)
     print(f"dataset item output shapes: {dataset_item[0].shape}, {dataset_item[1].shape}")
+
+    # import matplotlib.pyplot as plt
+    # plt.imshow(dataset_item[0][0, :, :], origin="lower")
+    # plt.imshow(dataset_item[1][1, :, :], origin="lower")
+    # plt.show()
 
 
 if __name__ == "__main__":
--- ocean_navigation_simulator/generative_error_model/GAN/train_model.py
+++ ocean_navigation_simulator/generative_error_model/GAN/train_model.py
@@ -7,8 +7,15 @@ import yaml
 import torch
 from torch.utils.data import DataLoader
 from torch import optim
+from torch.nn import functional as F
 from typing import Dict, Callable, Any
 from warnings import warn
+from tqdm import tqdm
+
+
+# TODO: overfit on single batch
+# TODO: vis fixed batch during course of training
+# TODO: verify loss @ init
 
 
 def get_model(model_type: str, model_configs: Dict, device: str) -> Callable:
@@ -22,81 +29,144 @@ def get_model(model_type: str, model_configs: Dict, device: str) -> Callable:
 
 
 def get_optimizer(model, name: str, args_optimizer: dict[str, Any], lr: float):
-    args_optimizer['lr'] = lr
+    args_optimizer['lr'] = float(lr)
+    print(f"Optimizer params: {args_optimizer}")
     if name.lower() == "adam":
         return optim.Adam(model.parameters(), **args_optimizer)
     raise warn("No optimizer!")
     return None
 
 
-def train():
-    # # log metrics inside loop.
-    # wandb.log({"loss": loss})
-    return 1
+def predict_fixed_batch():
+    # use Dataset directly to always access the correct data.
+    return
 
 
-def validation():
-    return 1
+def loss_function(predictions, target, type: str = "mse"):
+    """Handles which loss is to be used.
+    """
+    if type == "mse":
+        loss = F.mse_loss(predictions, target, reduction="mean")
+    elif type == "sparse_mse":
+        loss = torch.where(target != 0, (target**2 + predictions**2)/2, 0).sum()
+    return loss
+
+
+def train(model, dataloader, device, optimizer, cfgs_train):
+    total_loss = 0
+    model.train()
+    with torch.enable_grad():
+        with tqdm(dataloader, unit="batch") as tepoch:
+            for data, target in tepoch:
+                data, target = data.to(device), target.to(device)
+
+                output = model(data)
+
+                # compute loss
+                loss = loss_function(output, target, cfgs_train["loss_type"])
+                total_loss += loss.item()
+
+                # perform optim step
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                tepoch.set_postfix(loss=str(round(loss.item(), 3)))
+                wandb.log({"loss": loss.item()})
+
+    avg_loss = total_loss / len(dataloader)
+    print(f"Training avg loss: {avg_loss:.2f}.")
+
+    return avg_loss
+
+
+def validation(model, dataloader, device, cfgs_train):
+    total_loss = 0
+    model.eval()
+    with torch.no_grad():
+        with tqdm(dataloader, unit="batch") as tepoch:
+            for data, target in tepoch:
+                data, target = data.to(device), target.to(device)
+
+                output = model(data)
+                loss = loss_function(output, target, cfgs_train["loss_type"])
+                total_loss += loss.item()
+
+                tepoch.set_postfix(loss=str(round(loss.item(), 3)))
+
+    avg_loss = total_loss / len(dataloader)
+    print(f"Validation avg loss: {avg_loss:.2f}")
+    return avg_loss
 
 
 def clean_up_training():
+    # report best losses
+    # potential plots
     pass
 
 
 def main():
-    # wandb.init(project="Generative Models for Realistic Simulation of Ocean Currents",
-    #            entity="ocean-platform-control")
     parser = argparse.ArgumentParser()
     parser.add_argument("config_file", type=str, help="specify the file config for model and training")
-    config_file = parser.parse_args().file_configs
-    # wandb.config = load dict from yaml
+    config_file = parser.parse_args().config_file
     all_cfgs = yaml.load(open(config_file, "r"), Loader=yaml.FullLoader)
+    wandb_cfgs = {"mode": all_cfgs.get("wandb_mode", "online")}
+    wandb.init(project="Generative Models for Realistic Simulation of Ocean Currents",
+               entity="ocean-platform-control",
+               **wandb_cfgs)
+    wandb.config = all_cfgs
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Running on: {device}.")
 
+    # seed for reproducability
+    torch.manual_seed(0)
+
+    # simply config access
     model_type = all_cfgs["model"]
     cfgs_model = all_cfgs[model_type]
     cfgs_dataset = all_cfgs["dataset"]
     cfgs_train = all_cfgs["train"]
+    cfgs_optimizer = all_cfgs["optimizer"]
 
     # load training data
-    dataset = BuoyForecastError(cfgs_dataset["forecasts"], cfgs_dataset["ground_truth"])
-    dataset_len = dataset.__len__()
+    dataset = BuoyForecastError(cfgs_dataset["forecasts"], cfgs_dataset["ground_truth"], cfgs_dataset["len"])
+    print(f"Loading forecasts from {cfgs_dataset['forecasts']} and ground truth from {cfgs_dataset['ground_truth']}.")
+    dataset_len = len(dataset)
+    print(f"Dataset length: {dataset_len}.")
     train_set, val_set = torch.utils.data.random_split(dataset,
-                                                       [0.8*dataset_len, 0.2*dataset_len],
+                                                       [dataset_len-int(0.2*dataset_len), int(0.2*dataset_len)],
                                                        generator=torch.Generator().manual_seed(0))
     train_loader = DataLoader(dataset=train_set, batch_size=cfgs_train["batch_size"], shuffle=cfgs_dataset["shuffle"])
     val_loader = DataLoader(dataset=val_set, batch_size=cfgs_train["test_batch_size"], shuffle=cfgs_dataset["shuffle"])
 
     # define model
     print(f"Using: {model_type}.")
+    print(f"Using: {cfgs_train['loss_type']}.")
     model = get_model(model_type, cfgs_model, device)
 
-    optimizer = get_optimizer(model, "adam", {}, lr=cfgs_train["learning_rate"])
+    optimizer = get_optimizer(model, cfgs_optimizer["name"], cfgs_optimizer["parameters"], lr=cfgs_train["learning_rate"])
 
     train_losses, val_losses = list(), list()
     try:
         for epoch in range(1, cfgs_train["epochs"] + 1):
             metrics = {}
 
-            loss = train()
+            loss = train(model, train_loader, device, optimizer, cfgs_train)
             train_losses.append(loss)
             metrics |= {"train_loss": loss}
 
-            loss = validation()
-            val_losses.append(loss)
-            metrics |= {"val_loss": loss}
+            if len(val_loader) != 0:
+                loss = validation(model, val_loader, device, cfgs_train)
+                val_losses.append(loss)
+                metrics |= {"val_loss": loss}
 
             wandb.log(metrics)
+            print(f"Epoch metrics: {metrics}.")
 
     finally:
         clean_up_training()
 
 
-
-
 if __name__ == "__main__":
     main()
-
-
