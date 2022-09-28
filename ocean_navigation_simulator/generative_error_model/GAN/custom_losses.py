521ebf17e2cef0c2228c7b0d45afcef9ba9943ac
Jonas Dieker <jonasdieker05@gmail.com>
2022-09-28 13:47:04 -0700

    add losses

---
+++ ocean_navigation_simulator/generative_error_model/GAN/custom_losses.py
@@ -0,0 +1,20 @@
+import torch
+
+
+def sparse_mse(predictions, target):
+    return torch.where(target != 0, (target - predictions)**2/2, 0).sum()
+
+
+def total_variation(img, tv_weight: int = 1, reduction: str = "sum"):
+    """Compute total variation loss."""
+    w_variance = (img[:, :, :, :-1] - img[:, :, :, 1:]).abs()
+    h_variance = (img[:, :, :-1, :] - img[:, :, 1:, :]).abs()
+    if reduction == "sum":
+        w_variance = w_variance.sum()
+        h_variance = h_variance.sum()
+    elif reduction == "mean":
+        w_variance = w_variance.mean()
+        h_variance = h_variance.mean()
+
+    loss = tv_weight * (h_variance + w_variance)
+    return loss
