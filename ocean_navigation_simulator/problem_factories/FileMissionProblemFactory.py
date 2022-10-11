1bb2a4d29122f7b1ff1b1ac27bfdc496d4e64bd2
Jerome Jeannin <jeromemjeannin@gmail.com>
2022-10-10 18:55:43 -0700

    extended ProblemFactory

--- ocean_navigation_simulator/problem_factories/FileMissionProblemFactory.py
+++ ocean_navigation_simulator/problem_factories/FileMissionProblemFactory.py
@@ -11,37 +11,50 @@ class FileMissionProblemFactory(ProblemFactory):
         self,
         seed: Optional[int] = None,
         csv_file: Optional[str] = '/seaweed-storage/generation/gulf_of_mexico_HYCOM_hindcast/increased_planner_area/problems.csv',
+        indices: Optional[List] = None,
         limit: Optional[int] = None,
     ):
         self.seed = seed
         self.csv_file = csv_file
+        self.indices = indices
         self.limit = limit
 
-        if limit is not None:
-            self.problems_df = pd.read_csv(csv_file)
-        else:
-            self.problems_df = pd.read_csv(csv_file).head(n=limit)
+        self.problems_df = pd.read_csv(csv_file, index_col=0)
 
-        if seed is not None:
-            self.random = np.random.default_rng(seed)
-            self.indexes_available = self.random.permutation(self.problems_df.shape[0]).tolist()
-        else:
-            self.indexes_available = list(range(self.problems_df.shape[0]))
+        self._set_indices()
 
     def has_problems_remaining(self) -> bool:
         return len(self.indexes_available) > 0
 
     def get_problem_list(self, n = None) -> [NavigationProblem]:
-        return [self.next_problem() for _ in range(min(n, len(self.indexes_available)))]
+        if n is None:
+            return [self.next_problem() for _ in range(len(self.indexes_available))]
+        else:
+            return [self.next_problem() for _ in range(min(n, len(self.indexes_available)))]
 
     def next_problem(self) -> NavigationProblem:
         if not self.has_problems_remaining():
-            if self.limit is not None:
-                raise Exception("No more available Problems.")
+            if self.limit is None:
+                self._set_indices()
             else:
-                self.indexes_available = self.random.permutation(self.problems_df.shape[0]).tolist()
+                raise Exception("No more available Problems.")
 
         index = self.indexes_available.pop(0)
         row = self.problems_df.iloc[index]
 
         return NavigationProblem.from_pandas_row(row)
+
+    def _set_indices(self):
+        if self.indices is None:
+            if self.limit is None:
+                available = self.problems_df.shape[0]
+            else:
+                available = self.limit
+
+            if self.seed is None:
+                self.indexes_available = list(range(available))
+            else:
+                self.random = np.random.default_rng(self.seed)
+                self.indexes_available = self.random.permutation(available).tolist()
+        else:
+            self.indexes_available = self.indices
