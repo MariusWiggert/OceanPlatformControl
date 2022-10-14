5703617d8e59b71c158941f34cbef03da52c9a49
Jonas Dieker <jonasdieker05@gmail.com>
2022-10-14 13:56:43 -0700

    add weight initialization func

--- ocean_navigation_simulator/generative_error_model/GAN/utils.py
+++ ocean_navigation_simulator/generative_error_model/GAN/utils.py
@@ -1,5 +1,9 @@
 import torch
 from torch.nn import functional as F
+from torch.nn import init
+
+
+# _____________________________ LOSSES ___________________________________ #
 
 
 def sparse_mse(predictions, target):
@@ -47,6 +51,42 @@ def mass_conservation(tensor: torch.Tensor, scale: str = "locally"):
     elif scale == "globally":
         mc_loss = torch.abs(all_losses.sum())/(total_size - num_nans)
     return mc_loss
+
+
+# _____________________________ NET CONFIGS __________________________________ #
+
+
+def init_weights(net, init_type='normal', init_gain=0.02):
+    """Initialize network weights.
+    Parameters:
+        net (network)   -- network to be initialized
+        init_type (str) -- the name of an initialization method: normal | xavier | kaiming | orthogonal
+        init_gain (float)    -- scaling factor for normal, xavier and orthogonal.
+    """
+    def init_func(m):  # define the initialization function
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            if init_type == 'normal':
+                init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('Initialization method [%s] is not implemented' % init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant_(m.bias.data, 0.0)
+        elif classname.find('BatchNorm2d') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
+            init.normal_(m.weight.data, 1.0, init_gain)
+            init.constant_(m.bias.data, 0.0)
+
+    print('Initialize network with %s' % init_type)
+    net.apply(init_func)  # apply the initialization function <init_func>
+
+
+# _____________________________ SAVING __________________________________ #
 
 
 def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
