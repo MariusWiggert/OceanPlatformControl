115b6e59dceb1c53006f95f3c4356e295bcfbdad
Jonas Dieker <jonasdieker05@gmail.com>
2022-10-23 12:08:38 -0700

    add classes for simplex noise

---
+++ ocean_navigation_simulator/generative_error_model/models/OceanCurrentNoiseField.py
@@ -0,0 +1,176 @@
+from ocean_navigation_simulator.generative_error_model.models.SimplexNoiseModel import SimplexNoiseModel,\
+    HarmonicParameters
+from ocean_navigation_simulator.generative_error_model.models.GenerativeModel import GenerativeModel
+from ocean_navigation_simulator.generative_error_model.utils import convert_degree_to_km
+from ocean_navigation_simulator.generative_error_model.Problem import Problem
+from ocean_navigation_simulator.utils import units
+
+from typing import List, Dict, Any
+import xarray as xr
+import datetime
+import numpy as np
+
+
+class OceanCurrentNoiseField(GenerativeModel):
+    """Uses noise model to construct a field of noise values."""
+
+    def __init__(self, harmonic_params: Dict[str, Any], detrend_statistics: np.ndarray):
+        u_comp_harmonics = [HarmonicParameters(*harmonic) for harmonic in harmonic_params["U_COMP"]]
+        v_comp_harmonics = [HarmonicParameters(*harmonic) for harmonic in harmonic_params["V_COMP"]]
+        self.model = SimplexNoiseModel(u_comp_harmonics, v_comp_harmonics)
+        self.detrend_statistics = detrend_statistics
+
+    def reset(self, rng: np.random.default_rng) -> None:
+        """Initializes the simplex noise with a new random number generator."""
+        self.model.reset(rng)
+
+    def get_noise(self, problem: Problem) -> xr.Dataset:
+        """Uses the SimplexNoiseModel to produce a noise field over the specified ranges.
+        Assumes the origin is positioned in top left corner at timedelta=0 hrs."""
+
+        # TODO: user-defined resolutions, since they vary between HYCOM and Copernicus.
+        lon_res, lat_res, t_res = 1 / 12, 1 / 12, 1
+        lon_range, lat_range, t_range = problem.lon_range, problem.lat_range, problem.t_range
+
+        # drop timezome from datetime
+        t_range[0] = t_range[0].replace(tzinfo=None)
+        t_range[1] = t_range[1].replace(tzinfo=None)
+
+        # create axis steps
+        lon_locs = np.arange(lon_range[0], lon_range[1], lon_res)
+        lat_locs = np.arange(lat_range[0], lat_range[1], lat_res)
+        t_locs = timedelta_range_hours(datetime.timedelta(hours=0), t_range[1] - t_range[0])
+
+        noise = np.zeros((len(lon_locs), len(lat_locs), len(t_locs), 2))
+
+        # TODO: improve on prototype with nested for loops.
+        for i, lon in enumerate(lon_locs):
+            for j, lat in enumerate(lat_locs):
+                for k, elapsed_time in enumerate(t_locs):
+                    # need degrees relative to start of locs, convert to km and feed into noise model
+                    x, y = convert_degree_to_km(lon - lon_range[0], lat - lat_range[0])
+                    x_km = units.Distance(km=x)
+                    y_km = units.Distance(km=y)
+                    point_noise = self.model.get_noise(x_km, y_km, elapsed_time)
+                    noise[i, j, k, :] = np.array([float(point_noise.u.meters_per_second),
+                                                  float(point_noise.v.meters_per_second)])
+
+        # reintroduce trends into error
+        noise[:, :, :, 0] = noise[:, :, :, 0] * self.detrend_statistics[0, 1] + self.detrend_statistics[0, 0]
+        noise[:, :, :, 1] = noise[:, :, :, 1] * self.detrend_statistics[1, 1] + self.detrend_statistics[1, 0]
+
+        return self._create_xarray(noise, lon_locs, lat_locs, t_locs, t_range)
+
+    def get_noise_vec(self, problem: Problem) -> xr.Dataset:
+        """Uses the SimplexNoiseModel to produce a noise field over the specified ranges.
+        Assumes the origin is positioned in top left corner at timedelta=0 hrs."""
+
+        # TODO: user-defined resolutions, since they vary between HYCOM and Copernicus.
+        lon_res, lat_res, t_res = 1/12, 1/12, 1
+        lon_range, lat_range, t_range = problem.lon_range, problem.lat_range, problem.t_range
+
+        # drop timezome from datetime
+        t_range[0] = t_range[0].replace(tzinfo=None)
+        t_range[1] = t_range[1].replace(tzinfo=None)
+
+        # create axis steps
+        lon_locs = np.arange(lon_range[0], lon_range[1], lon_res)
+        lat_locs = np.arange(lat_range[0], lat_range[1], lat_res)
+        t_locs = np.array(timedelta_range_hours(datetime.timedelta(hours=0), t_range[1] - t_range[0]))
+        # convert to numpy otherwise numba throws a fit! # possible to do timedelta.to_timedelta64()
+        t_locs = np.array([np.timedelta64(timedelta) for timedelta in t_locs])
+
+        # get a row of const lat and varying lon and time.
+        # Need to convert from degrees to km first. Cant sample all position at once because degree space
+        # is not Euclidean and opensimplex only allows for axis sampling. Therefore, sample one plane of lon
+        # and time pairs at a time for which lat is const.
+        noise = np.zeros((len(lon_locs), len(lat_locs), len(t_locs), 2))
+        for i, lat_loc in enumerate(lat_locs):
+            # constant latitude for conversion of lon values
+            lat_pos = np.full(len(lon_locs), lat_loc)
+            x_km, y_km = convert_degree_to_km(lon_locs - lon_range[0], lat_pos - lat_range[0])
+            y_km = np.array([y_km[0]])
+
+            noise[:, i, :, :] = np.squeeze(self.model.get_noise_vec(x_km, y_km, t_locs), axis=1)
+
+        # reintroduce trends into error
+        noise = noise.reshape(len(lon_locs), len(lat_locs), len(t_locs), 2)
+
+        # print(f"u metrics: {noise[:, :, :, 0].mean()}, {np.sqrt(noise[:, :, :, 0].var())}")
+        # print(f"v metrics: {noise[:, :, :, 1].mean()}, {np.sqrt(noise[:, :, :, 1].var())}")
+        # # what we want:
+        # print(self.detrend_statistics[0, 0], self.detrend_statistics[0, 1])
+        # print(self.detrend_statistics[1, 0], self.detrend_statistics[1, 1])
+
+        noise[:, :, :, 0] = noise[:, :, :, 0] * self.detrend_statistics[0, 1] + self.detrend_statistics[0, 0]
+        noise[:, :, :, 1] = noise[:, :, :, 1] * self.detrend_statistics[1, 1] + self.detrend_statistics[1, 0]
+
+        # print(f"u metrics: {noise[:, :, :, 0].mean()}, {np.sqrt(noise[:, :, :, 0].var())}")
+        # print(f"v metrics: {noise[:, :, :, 1].mean()}, {np.sqrt(noise[:, :, :, 1].var())}")
+
+        # need to convert time to list
+        t_locs = t_locs.tolist()
+
+        return self._create_xarray(noise, lon_locs, lat_locs, t_locs, t_range)
+
+    def _create_xarray(self, data: np.ndarray, lon_locs: np.ndarray, lat_locs: np.ndarray,
+                       t_locs: List[datetime.timedelta], t_range: List[datetime.datetime]):
+
+        ds = xr.Dataset(
+            data_vars=dict(
+                u_error=(["lon", "lat", "time"], data[:, :, :, 0]),
+                v_error=(["lon", "lat", "time"], data[:, :, :, 1]),
+            ),
+            coords=dict(
+                lon=lon_locs,
+                lat=lat_locs,
+                time=datetime_range_from_timedeltas(t_range[0], t_locs)
+            ),
+            attrs=dict(description="An ocean current error sample over time and space.")
+        )
+        return ds
+
+
+def datetime_range_from_timedeltas(start: datetime.datetime, timedeltas: List[datetime.timedelta]):
+    return [start + timedelta for timedelta in timedeltas]
+
+
+def timedelta_range_hours(start: datetime.timedelta, end: datetime.timedelta):
+    start_hours = timedelta_to_hours(start)
+    end_hours = timedelta_to_hours(end)
+    return [start + datetime.timedelta(hours=n) for n in range(int(end_hours-start_hours))]
+
+
+def timedelta_to_hours(timedelta: datetime.timedelta):
+    return timedelta.days*24 + timedelta.seconds//3600
+
+
+def test(save_path):
+    params_path = "tuned_2d_forecast_variogram_area1_[5.0, 1.0]_False_True.npy"
+    parameters = np.load(params_path, allow_pickle=True)
+
+    # define the components instead of receiving them from OceanCurrentNoiseField
+    harmonic_params = {"U_COMP": parameters.item().get("U_COMP"),
+                       "V_COMP": parameters.item().get("V_COMP")}
+    detrend_stats = parameters.item().get("detrend_metrics")
+
+    noise_field = OceanCurrentNoiseField(harmonic_params, np.array(detrend_stats))
+    rng = np.random.default_rng(21)  # try different seeds to see if deterministic
+    noise_field.reset(rng)
+
+    # define the problem
+    lon_range = [-140, -120]
+    lat_range = [20, 30]
+    t_range = [datetime.datetime(2022, 4, 21, 12, 30, 0),
+               datetime.datetime(2022, 5, 21, 12, 30, 0)]
+    problem = Problem(lon_range, lat_range, t_range)
+
+    # get the noise
+    noise_field = noise_field.get_noise_vec(problem)
+    print(noise_field)
+    noise_field.to_netcdf(save_path)
+
+
+if __name__ == "__main__":
+    save_path = "sample_noise.nc"
+    test(save_path)
---
+++ ocean_navigation_simulator/generative_error_model/models/SimplexNoiseModel.py
@@ -0,0 +1,283 @@
+# Vectorized and adapted version of:
+# https://github.com/google/balloon-learning-environment/blob/master/balloon_learning_environment/env/wind_field.py
+from ocean_navigation_simulator.utils import units
+
+import dataclasses
+import opensimplex
+import numpy as np
+import datetime
+from typing import List
+
+
+@dataclasses.dataclass(frozen=True)
+class HarmonicParameters(object):
+    """Parameters for noise harmonic."""
+    weight: float
+    x_range: float
+    y_range: float
+    time_range: float
+
+
+@dataclasses.dataclass(frozen=True)
+class SimplexOffset(object):
+    """Defines a displacement from the base simplex grid."""
+    x: float
+    y: float
+    time: float
+
+
+@dataclasses.dataclass(frozen=True)
+class CurrentVector(object):
+    """Describes the wind at a given location."""
+    u: units.Velocity
+    v: units.Velocity
+
+    def add(self, other: 'CurrentVector') -> 'CurrentVector':
+        if not isinstance(other, CurrentVector):
+            raise NotImplementedError(
+                f'Cannot add CurrentVector with {type(other)}')
+        return CurrentVector(self.u + other.u, self.v + other.v)
+
+    def __str__(self) -> str:
+        return f'({self.u}, {self.v})'
+
+
+# OpenSimplex variance for a 3D noise field
+OPENSIMPLEX_VARIANCE = 0.0979
+# The noise's variance should match the global variance. Therefore, the
+# noise is scaled.
+NOISE_MAGNITUDE = np.sqrt(1.02/OPENSIMPLEX_VARIANCE)
+
+
+class SimplexNoiseModel:
+    """Noise model which based on simplex noise and parameters found through
+    variogram analysis will generate noise at a point or over a volume"""
+
+    def __init__(self, u_comp_harmonics: List[HarmonicParameters], v_comp_harmonics: List[HarmonicParameters]):
+        self.noise_u = NoisyCurrentComponent(u_comp_harmonics)
+        self.noise_v = NoisyCurrentComponent(v_comp_harmonics)
+
+    def reset(self, rng) -> None:
+        noise_u_rng, noise_v_rng = rng.choice(1894405231, size=2)
+        noise_u_rng = np.random.default_rng(noise_u_rng)
+        noise_v_rng = np.random.default_rng(noise_v_rng)
+        self.noise_u.reset(noise_u_rng)
+        self.noise_v.reset(noise_v_rng)
+
+    def get_noise(self, x: units.Distance, y: units.Distance,
+                  elapsed_time: datetime.timedelta) -> CurrentVector:
+        noise_u_value = self.noise_u.get_noise(x, y, elapsed_time)
+        noise_v_value = self.noise_v.get_noise(x, y, elapsed_time)
+
+        return CurrentVector(
+            units.Velocity(meters_per_second=noise_u_value),
+            units.Velocity(meters_per_second=noise_v_value)
+        )
+
+    def get_noise_vec(self, x: np.ndarray, y: np.ndarray, elapsed_time: np.ndarray) -> np.ndarray:
+        noise_u_value = self.noise_u.get_noise_vec(x, y, elapsed_time)
+        noise_v_value = self.noise_v.get_noise_vec(x, y, elapsed_time)
+
+        # reshape noise into x,y,z
+        # noise_u_value = np.moveaxis(noise_u_value, [2, 1, 0], [0, 1, 2])
+        # noise_v_value = np.moveaxis(noise_v_value, [2, 1, 0], [0, 1, 2])
+
+        noise = np.array([noise_u_value, noise_v_value])
+        noise = np.moveaxis(noise, list(range(0, len(noise.shape))), list(range(len(noise.shape)-1, -1, -1)))
+        return noise
+
+
+class NoisyCurrentComponent:
+    """Computed from multiple NoisyCurrentHarmonics."""
+
+    def __init__(self, comp_harmonics: List[HarmonicParameters]):
+
+        harmonic_params = comp_harmonics
+
+        # create list of harmonics
+        self._harmonics = [
+            NoisyCurrentHarmonic(params) for params in harmonic_params
+        ]
+
+    def reset(self, rng) -> None:
+        """Create new rng for each harmonic from original rng."""
+        num_harmonics = len(self._harmonics)
+        harmonics_rngs_seeds = rng.choice(1894405231, size=num_harmonics)
+        harmonics_rngs = [np.random.default_rng(seed) for seed in harmonics_rngs_seeds]
+
+        for rng, harmonic in zip(harmonics_rngs, self._harmonics):
+            harmonic.reset(rng)
+
+    def get_noise(self, x: units.Distance, y: units.Distance, elapsed_time: datetime.timedelta) -> float:
+        """Returns simplex noise at defined location (in space and time), for specified components."""
+
+        weighted_noise = 0.0
+        total_weight = 0.0
+        total_weight_squared = 0.0
+
+        # Sum noise from all harmonics and weight
+        for harmonic in self._harmonics:
+            noise = harmonic.get_noise(x, y, elapsed_time)
+            weighted_noise += noise * harmonic.weight
+            total_weight += harmonic.weight
+            total_weight_squared += harmonic.weight**2
+
+        # Scale to recover the desired empirical variance
+        weighted_noise /= total_weight
+        weighted_noise *= np.sqrt(total_weight / total_weight_squared)
+
+        return weighted_noise
+
+    def get_noise_vec(self, x: np.ndarray, y: np.ndarray, elapsed_time: np.ndarray) -> np.ndarray:
+        """Returns simplex noise at defined location (in space and time), for specified components."""
+
+        weighted_noise = np.zeros((x.squeeze().shape, y.squeeze().shape, elapsed_time.squeeze().shape))
+        total_weight = 0.0
+        total_weight_squared = 0.0
+
+        # Sum noise from all harmonics and weight
+        for harmonic in self._harmonics:
+            noise = harmonic.get_noise_vec(x, y, elapsed_time)
+            weighted_noise += noise * harmonic.weight
+            total_weight += harmonic.weight
+            total_weight_squared += harmonic.weight**2
+
+        # Scale to recover the desired empirical variance
+        # Note: Dont immediately multiply by sqrt of weight in loop because want to normalize weight
+        weighted_noise /= total_weight
+        # Note: need to scale in this way because actual weight is proportional to sill (harmonic weight)
+        weighted_noise *= np.sqrt(total_weight / total_weight_squared)
+
+        return weighted_noise
+
+
+class NoisyCurrentHarmonic:
+    """Sub-component of NoisyCurrentComponent. Multiple weighted NoisyCurrentHarmonics
+    make up a NoisyCurrentComponent."""
+
+    def __init__(self, params: HarmonicParameters):
+        self._simplex_generator = None
+        self._offsets = None
+
+        # unpacking harmonic components
+        self.weight = params.weight
+        self.x_range = params.x_range
+        self.y_range = params.y_range
+        self.time_range = params.time_range
+
+    def reset(self, rng) -> None:
+        """Resets harmonic's noise."""
+        current_seed = rng.choice(1894405231)
+        self._simplex_generator = opensimplex.OpenSimplex(seed=current_seed)
+
+        # OpenSimplex always return zero at origin so for a given
+        # generated noise a random offset from the origin is used
+
+        random_translation = rng.uniform(size=(3,)) * 2.0 - 1.0
+        self._offsets = SimplexOffset(*random_translation)
+
+    def get_noise(self, x: units.Distance, y: units.Distance, elapsed_time: datetime.timedelta) -> float:
+        """Returns simplex noise for this harmonic at specific point in time and space."""
+
+        if self._simplex_generator is None:
+            raise ValueError("Must call reset before get_noise.")
+
+        time_in_hours = units.timedelta_to_hours(elapsed_time)
+
+        return NOISE_MAGNITUDE * self._simplex_generator.noise3(
+            x.km / self.x_range + self._offsets.x,
+            y.km / self.y_range + self._offsets.y,
+            time_in_hours / self.time_range + self._offsets.time)
+
+    def get_noise_vec(self, x: np.ndarray, y: np.ndarray, elapsed_times: np.ndarray) -> np.ndarray:
+        """Returns simplex noise for this harmonic at specific point in time and space."""
+
+        if self._simplex_generator is None:
+            raise ValueError("Must call reset before get_noise.")
+
+        time_in_hours = np.array([elapsed_time.astype('timedelta64[h]')/np.timedelta64(1, 'h')
+                                  for elapsed_time in elapsed_times])
+
+        # magnitude is needed to scale the noise to a variance of 1.
+        return NOISE_MAGNITUDE * self._simplex_generator.noise3array(
+            x / self.x_range + self._offsets.x,
+            y / self.y_range + self._offsets.y,
+            time_in_hours / self.time_range + self._offsets.time)
+
+
+def test():
+
+    # define the components instead of receiving them from OceanCurrentNoiseField
+    u_comp = [
+        HarmonicParameters(0.5, 702.5, 1407.3, 245.0),
+        HarmonicParameters(0.5, 302.5, 1207.3, 187.0)]
+    v_comp = [
+        HarmonicParameters(0.5, 702.5, 1407.3, 245.0),
+        HarmonicParameters(0.5, 302.5, 1207.3, 187.0)]
+
+    rng = np.random.default_rng(2022)
+    new_seeds = rng.choice(10000, 2)
+    rng1 = np.random.default_rng(new_seeds[0])
+    rng2 = np.random.default_rng(new_seeds[1])
+
+    noise_model = SimplexNoiseModel(u_comp, v_comp)
+    noise_model.reset(rng1)
+    velocity1 = noise_model.get_noise(
+        units.Distance(km=200),
+        units.Distance(km=300),
+        datetime.timedelta(days=5))
+
+    velocity2 = noise_model.get_noise(
+        units.Distance(km=200),
+        units.Distance(km=300),
+        datetime.timedelta(days=5))
+
+    noise_model.reset(rng2)
+    velocity3 = noise_model.get_noise(
+        units.Distance(km=200),
+        units.Distance(km=300),
+        datetime.timedelta(days=5)
+    )
+
+    assert(velocity1 == velocity2), "SimplexNoiseModel not the same for same seed!"
+    assert(velocity1 != velocity3), "SimplexNoiseModel the same for different seed!"
+
+
+def test_equal_methods_simplexnoisemodel():
+    # define the components instead of receiving them from OceanCurrentNoiseField
+    u_comp = [
+        HarmonicParameters(0.5, 702.5, 1407.3, 245.0),
+        HarmonicParameters(0.5, 302.5, 1207.3, 187.0)
+    ]
+    v_comp = [
+        HarmonicParameters(0.5, 702.5, 1407.3, 245.0),
+        HarmonicParameters(0.5, 302.5, 1207.3, 187.0)
+    ]
+
+    noise = SimplexNoiseModel(u_comp, v_comp)
+    noise.reset(np.random.default_rng(123))
+    elapsed_times = np.array([np.timedelta64(5, 'h'), np.timedelta64(6, 'h'), np.timedelta64(20, 'h')])
+    output = noise.get_noise_vec(np.array([200, 300, 500, 550]), np.array([300, 200, 100]), elapsed_times)
+    # select u only
+    vec_output = output[:, :, :, 0]
+
+    # non-vectorized method
+    x_pos = [200, 300, 500, 550]
+    y_pos = [300, 200, 100]
+    time = [5, 6, 20]
+    for i, x in enumerate(x_pos):
+        for j, y in enumerate(y_pos):
+            for k, t in enumerate(time):
+                output = noise.get_noise(units.Distance(km=x),
+                                         units.Distance(km=y),
+                                         datetime.timedelta(hours=t))
+                # print(output.u.meters_per_second)
+                assert round(output.u.meters_per_second, 4) == round(vec_output[i, j, k], 4), "Methods are not equal!"
+
+    print("methods are equal!")
+
+
+if __name__ == "__main__":
+    test()
+    test_equal_methods_simplexnoisemodel()
+    print("All tests passed.")
