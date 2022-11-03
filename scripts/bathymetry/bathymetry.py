defffd4b10a9f09eaa886d6028a74c3dd5dafef2
Andreas <35858164+a-doering@users.noreply.github.com>
2022-11-03 14:51:13 -0700

    Refactor and document

--- scripts/bathymetry/bathymetry.py
+++ scripts/bathymetry/bathymetry.py
@@ -1,28 +1,77 @@
 import cartopy.crs as ccrs
+import dask
 import matplotlib.pyplot as plt
 import numpy as np
 import plotly.graph_objects as go
 import xarray as xr
-import dask
 
 
-def format_to_equally_spaced_xy_grid(xarray):
-    """Helper Function to format an xarray to equally spaced lat, lon axis."""
-    xarray["lon"] = np.linspace(
-        xarray["lon"].data[0], xarray["lon"].data[-1], len(xarray["lon"].data)
-    )
-    xarray["lat"] = np.linspace(
-        xarray["lat"].data[0], xarray["lat"].data[-1], len(xarray["lat"].data)
-    )
-    return xarray
+def coarsen(
+    filename: str, res_lat: float = 1 / 12, res_lon: float = 1 / 12, op="max"
+) -> xr.Dataset():
+    """Coarsen the global bathymetry map to lower resolution.
+
+    Args:
+        filename (str): Global gebco bathymetry map path
+        res_lat (float, optional): Target resolution lat in degree. Defaults to 1/12.
+        res_lon (float, optional): Target resolution lon in degree. Defaults to 1/12.
+        op (str, optional): Operation of coarsening. Defaults to "max".
+
+    Raises:
+        NotImplementedError: If operation that is not defined is passed.
+
+    Returns:
+        xr.Dataset: Coarsened dataset
+    """
+    ds = xr.open_dataset(filename, chunks={"lat": 8640, "lon": 4320})
+    ds["elevation"] = ds["elevation"].astype("float32")
+    # Resolution gebco = 1/240 th degree = 1/4 min
+    # To get to e.g. 1/12 degree resolution, we need to combine 1/12 * 240 = 20 points in one
+    # TODO: how to inform about rounding in case we need to round?
+    lat_coarsening = int(np.rint(res_lat * 240))
+    lon_coarsening = int(np.rint(res_lon * 240))
+    # Suppress dask warning about chunking size
+    print("Starting to coarsen the dataset. This may take a minute.")
+    with dask.config.set(**{"array.slicing.split_large_chunks": False}):
+        if op == "mean":
+            coarsened = ds.coarsen(lat=lat_coarsening, lon=lon_coarsening, boundary="exact").mean()
+        elif op == "max":
+            coarsened = ds.coarsen(lat=lat_coarsening, lon=lon_coarsening, boundary="exact").max()
+        else:
+            raise NotImplementedError("Unknown operation")
+    coarsened = coarsened.compute()
+    return coarsened
 
 
-def format_spatial_resolution(
-    xarray: xr.DataArray(), res_lat=1 / 12, res_lon=1 / 12, new_lat=None, new_lon=None
-):
-    if new_lat is None or new_lon is None:
-        new_lon = np.arange(xarray.lon[0], xarray.lon[-1], res_lon)
-        new_lat = np.arange(xarray.lat[0], xarray.lat[-1], res_lat)
+def generate_global_bathymetry_maps(
+    filename: str, res_lat: float = 1 / 12, res_lon: float = 1 / 12, op="max"
+) -> None:
+    """Generate and save global bathymetry map in lower resolution.
+
+    Args:
+        filename (str): Global gebco bathymetry map path
+        res_lat (float, optional): Target resolution lat in degree. Defaults to 1/12.
+        res_lon (float, optional): Target resolution lon in degree. Defaults to 1/12.
+        op (str, optional): Operation of coarsening. . Defaults to "max".
+    """
+    low_res = coarsen(filename, res_lat, res_lon, op)
+    low_res.to_netcdf(f"data/bathymetry/bathymetry_global_res_{res_lat:.3f}_{res_lon:.3f}_{op}.nc")
+    print("Saved global bathymetry map with lower resolution")
+
+
+def format_spatial_resolution(xarray: xr.Dataset(), res_lat=1 / 12, res_lon=1 / 12) -> xr.Dataset():
+    """Format the spatial resolution.
+
+    Args:
+        filename (str): Global gebco bathymetry map path
+        res_lat (float, optional): Target resolution lat in degree. Defaults to 1/12.
+        res_lon (float, optional): Target resolution lon in degree. Defaults to 1/12.
+    Returns:
+        xr.Dataset: Reduced resolution dataset
+    """
+
+    new_lon = np.arange(xarray.lon[0], xarray.lon[-1], res_lon)
+    new_lat = np.arange(xarray.lat[0], xarray.lat[-1], res_lat)
     xarray_new = xarray.interp(
         lat=new_lat,
         lon=new_lon,
@@ -34,40 +83,55 @@ def format_spatial_resolution(
     return xarray_new
 
 
-def coarsen(
-    filename: str, res_lat: float = 1 / 12, res_lon: float = 1 / 12, op="mean"
-) -> xr.Dataset():
-    """Generate a coarsened global bathymetry map, original resolution is 1/240 degree."""
-    ds = xr.open_dataset(filename, chunks={"lat": 8640, "lon": 4320})
-    ds["elevation"] = ds["elevation"].astype("float32")
-
-    # Resolution gebco = 1/240 th degree = 1/4 min
-    # To get to e.g. 1/12 degree resolution, we need to combine 1/12 * 240 = 20 points in one
-    # TODO: how to inform about rounding in case we need to round?
-    lat_coarsening = int(np.rint(res_lat * 240))
-    lon_coarsening = int(np.rint(res_lon * 240))
-    # Suppress dask warning about chunking size
-    with dask.config.set(**{"array.slicing.split_large_chunks": False}):
-        if op == "mean":
-            coarsened = ds.coarsen(lat=lat_coarsening, lon=lon_coarsening, boundary="exact").mean()
-        elif op == "max":
-            coarsened = ds.coarsen(lat=lat_coarsening, lon=lon_coarsening, boundary="exact").max()
-        else:
-            raise NotImplementedError("Unknown operation")
-
-    # TODO: how to do the coord_func ? mean, max, ...? Why apply mean at end instead of max in function
-    coarsened = coarsened.compute()
-    return coarsened
+def plot_bathymetry_2d(xarray: xr.Dataset()) -> None:
+    """Generate 2d plot with colorscale."""
+    xarray["elevation"].plot()
+    plt.show()
 
 
-# 3d plot
-# TODO: Needs adjustment of aspects
-def plot_bathymetry_3d(f, plot_sealevel=False):
+def plot_bathymetry_2d_levels(
+    xarray: xr.Dataset(), depth_decomposition: float = -1500, depth_min: float = -150
+) -> None:
+    """Generate 2d plot with only 3 levels: deep enough for decomposition,
+    deep enough for navigation, but not decomposition, too shallow.
 
-    x = f.variables["lon"]
-    y = f.variables["lat"]
-    z = f.variables["elevation"]
+    Args:
+        xarray (xr.Dataset): Input bathymetry map
+        depth_decomposition (float, optional): Minimal depth for decomposition. Defaults to -1500.
+        depth_min (float, optional): Minimal depth we want to be in. Defaults to -150.
+    """
+    xarray["elevation"].plot(levels=[depth_decomposition, depth_min])
+    plt.show()
 
+
+def plot_bathymetry_orthographic(
+    xarray: xr.Dataset(), lat_center=20, lon_center=-150, depth_decomposition=-1500, depth_min=-150
+):
+    """Plot bathymetry in 2d with orthographic projection.
+
+    Args:
+        xarray (xr.Dataset): Input bathymetry map
+        lat_center (int, optional): Center of view lat. Defaults to 20.
+        lon_center (int, optional): Center of view lon. Defaults to -150.
+        depth_decomposition (float, optional): Minimal depth for decomposition. Defaults to -1500.
+        depth_min (float, optional): Minimal depth we want to be in. Defaults to -150.
+    """
+    p = xarray["elevation"].plot(
+        levels=[depth_decomposition, depth_min],
+        subplot_kws=dict(projection=ccrs.Orthographic(lon_center, lat_center), facecolor="gray"),
+        transform=ccrs.PlateCarree(),
+    )
+    p.axes.set_global()
+    p.axes.coastlines()
+    plt.show()
+
+
+def plot_bathymetry_3d(xarray: xr.Dataset(), plot_sealevel: bool = False):
+    """Generate interactive 3d plot in browser."""
+    x = xarray.variables["lon"]
+    y = xarray.variables["lat"]
+    z = xarray.variables["elevation"]
+    # TODO: colorscale can be improved
     if plot_sealevel:
         sea_level = np.zeros_like(z)
 
@@ -75,7 +139,7 @@ def plot_bathymetry_3d(f, plot_sealevel=False):
             data=[go.Surface(z=z, x=x, y=y), go.Surface(z=sea_level, x=x, y=y, opacity=0.9)]
         )
     else:
-        fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
+        fig = go.Figure(data=[go.Surface(z=z, x=x, y=y, colorscale="Earth")])
     fig.update_traces(
         contours_z=dict(show=True, usecolormap=True, highlightcolor="limegreen", project_z=True)
     )
@@ -115,60 +179,16 @@ def plot_bathymetry_3d(f, plot_sealevel=False):
     fig.show()
 
 
-def plot_bathymetry_2d_cmap(f):
-    f["elevation"].plot(cmap="jet")
-    plt.show()
-
-
-def plot_bathymetry_2d_levels(f, depth_decomposition=-1500, depth_min=-30):
-    # Depth map with levels set
-    f["elevation"].plot(levels=[depth_decomposition, depth_min])
-    plt.show()
-
-
-def plot_bathymetry_orthographic(
-    f, lat_center=20, lon_center=-150, depth_decomposition=-1500, depth_min=-30
-):
-    # Plot elevation on world map with gray background
-    p = f["elevation"].plot(
-        levels=[depth_decomposition, depth_min],
-        subplot_kws=dict(projection=ccrs.Orthographic(lon_center, lat_center), facecolor="gray"),
-        transform=ccrs.PlateCarree(),
-    )
-    p.axes.set_global()
-    p.axes.coastlines()
-    plt.show()
-
-
-def generate_global_bathymetry_maps(
-    filename: str, res_lat: float = 1 / 12, res_lon: float = 1 / 12
-):
-    high_res = xr.open_dataset(filename)
-    low_res = coarsen(high_res, res_lat, res_lon)
-    low_res.to_netcdf(f"data/bathymetry/bathymetry_global_res_{res_lat:.3f}_{res_lon:.3f}.nc")
-
-
 if __name__ == "__main__":
+    # Generate global bathymetrymap and save to disk
     gebco_global_filename = "data/bathymetry/GEBCO_2022.nc"
-    arr = coarsen(gebco_global_filename)
-    arr["elevation"].plot()
-    plt.show()
+    generate_global_bathymetry_maps(gebco_global_filename)
 
-    # generate_global_bathymetry_maps(gebco_global_filename)
-
-    # generate_global_bathymetry_maps(gebco_global_fname, 1 / 12, 1 / 12)
-
-    # f = xr.open_dataset(gebco_global_fname)
-    # y_range = [f.variables["lat"].data[0], f.variables["lat"].data[-1]]
-    # x_range = [f.variables["lon"].data[0], f.variables["lon"].data[-1]]
-    # print(f)
-    # print(y_range, x_range)
-    # print(f.sel(lat=slice(20, 21)))
-
-    # depth_min = -30
-    # depth_decomposition = -1500
-
-    # # convert to binary
-    # area_forbidden = f.variables["elevation"] > depth_min
-    # area_floatable = np.logical_not(f.variables["elevation"])
-    # area_decomposition = f.variafilenamebles["elevation"] < -1500
+    # Test all other functions
+    # low_res_loaded = xr.open_dataset("data/bathymetry/bathymetry_global_res_0.083_0.083_max.nc")
+    # plot_bathymetry_orthographic(low_res_loaded)
+    # plot_bathymetry_2d_levels(low_res_loaded)
+    # plot_bathymetry_2d(low_res_loaded)
+    # Low res is still too large for 3d interactive plot
+    # very_low_res = format_spatial_resolution(low_res_loaded, res_lat=1 / 4, res_lon=1 / 4)
+    # plot_bathymetry_3d(very_low_res)
