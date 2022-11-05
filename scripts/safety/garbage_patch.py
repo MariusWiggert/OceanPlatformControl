1d3cc6efc84b2d417dab0210cb7abda769ccb43f
Andreas <35858164+a-doering@users.noreply.github.com>
2022-11-05 11:32:53 -0800

    Add convex hull

--- scripts/safety/garbage_patch.py
+++ scripts/safety/garbage_patch.py
@@ -1,8 +1,10 @@
-import pandas as pd
-import xarray as xr
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+from scipy.spatial import ConvexHull
 
 
 # TODO: replace with the function form datasource
@@ -20,20 +22,35 @@ def set_up_geographic_ax() -> plt.axes:
     return ax
 
 
-# TODO: see if this is indeed best dataset
-df = pd.read_csv("data/lebreton/Lebreton2018_HistoricalDataset.csv")
-df["lat"] = df["Latitude (degrees)"]
-df["lon"] = df["Longitude (degrees)"]
-df = df[df["Inside GPGP?"] == 1]
-ds = xr.Dataset.from_dataframe(df)
+def plot_convex_hull(points, ax):
+    """Plot a convex hull"""
+    hull = ConvexHull(points)
+    plt.plot(points[:, 0], points[:, 1], "o")
+    for simplex in hull.simplices:
+        plt.plot(points[simplex, 0], points[simplex, 1], "k-")
+    plt.plot(points[hull.vertices, 0], points[hull.vertices, 1], "r--", lw=2)
+    plt.plot(points[hull.vertices[0], 0], points[hull.vertices[0], 1], "ro")
+    plt.show()
 
-# Plotting with cartopy
-central_lat = 30
-central_lon = -145
-ax = plt.axes(projection=ccrs.PlateCarree())
-plt.title("Australia")
-ax.set_extent([-175, -100, 15, 45], ccrs.PlateCarree())
-ax.coastlines(resolution="110m")
-ax.scatter(ds["lon"], ds["lat"])
-# plt.plot(central_lon, central_lat, markersize=2, marker="o", color="red")
-plt.show()
+
+if __name__ == "__main__":
+    # TODO: see if this is indeed best dataset
+    df = pd.read_csv("data/lebreton/Lebreton2018_HistoricalDataset.csv")
+    df["lat"] = df["Latitude (degrees)"]
+    df["lon"] = df["Longitude (degrees)"]
+    df = df[df["Inside GPGP?"] == 1]
+    ds = xr.Dataset.from_dataframe(df)
+
+    # Plot a convex hull around garbage patch
+    ax = set_up_geographic_ax()
+    points = np.column_stack((ds["lon"], ds["lat"]))
+    plot_convex_hull(points, ax)
+
+    # # Plotting with cartopy
+    # central_lat = 30
+    # central_lon = -145
+    # ax = plt.axes(projection=ccrs.PlateCarree())
+    # ax.set_extent([-175, -100, 15, 45], ccrs.PlateCarree())
+    # ax.coastlines(resolution="110m")
+    # ax.scatter(ds["lon"], ds["lat"])
+    # plt.show()
