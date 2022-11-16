7c8502b22c3006eccba6fc20d0341a9629fd03c1
Andreas <35858164+a-doering@users.noreply.github.com>
2022-11-15 18:31:52 -0800

    Add DataSource2d and make BathymetrySource2d inherit from it

--- ocean_navigation_simulator/data_sources/DataSource2d.py
+++ ocean_navigation_simulator/data_sources/DataSource2d.py
@@ -1,192 +1,52 @@
-import datetime
+import abc
 import logging
-import os
 import time
-from typing import AnyStr, Dict, List, Optional, Tuple, Union
-
-import casadi as ca
-
-import cmocean
-import matplotlib.pyplot
-import matplotlib.pyplot as plt
-import numpy as np
 import xarray as xr
+from typing import AnyStr, List, Optional, Tuple, Union, Dict
 
 from ocean_navigation_simulator.environment.PlatformState import (
     PlatformState,
     SpatialPoint,
 )
+import matplotlib.pyplot
+import matplotlib.pyplot as plt
+import numpy as np
 
-# from ocean_navigation_simulator.utils import units
-from ocean_navigation_simulator.data_sources.DataSource2d import DataSource2d
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 
 
-class BathymetrySource2d(DataSource2d):
+class DataSource2d(abc.ABC):
+    """Abstract base class for time invariant data sources."""
+
+    logger: logging.Logger = logging.getLogger("data_source_2d")
+    # Casadi function needs to be inherited in child class
+
     def __init__(self, source_dict: Dict):
-        self.elevation_func = None  # Casadi function
-        super().__init__(source_dict)
-        self.logger = logging.getLogger("areana.bathymetry_source_2d")
-        self.logger.setLevel(os.environ.get("LOGLEVEL", "INFO").upper())
-
-    def instantiate_source_from_dict(self) -> None:
-        if self.source_dict["source"] == "gebco":
-            self.DataArray = self.get_DataArray_from_file()
-            self.grid_dict = self.get_grid_dict_from_xr(self.DataArray)
-        else:
-            raise NotImplementedError(
-                f"Selected source {self.source_dict['source']} in the BathymetrySource dict is not implemented."
-            )
-
-    def get_DataArray_from_file(self) -> xr:
-        DataArray = xr.open_dataset(self.source_dict["source_settings"]["filepath"])
-        # DataArray = DataArray.rename({"latitude": "lat", "longitude": "lon"})
-        return DataArray
-
-    def get_data_at_point(self, spatial_point: SpatialPoint) -> float:
-        # TODO: Watch for order lon, lat!
-        return self.elevation_func(spatial_point.__array__()[::-1])
-
-    def initialize_casadi_functions(self, grid: List[List[float]], array: xr) -> None:
-        """DataSource specific function to initialize the casadi functions needed.
-        # Note: the input to the casadi function needs to be an array of the form np.array([lat, lon])
-        Args:
-          grid:     list of the 2 grids [y_grid, x_grid] for the xr data
-          array:    xarray object containing the sub-setted data for the next cached round
-        """
-
-        self.elevation_func = ca.interpolant(
-            "elevation", "linear", grid, array["elevation"].values.ravel(order="F")
-        )
-
-    def is_higher_than(self, point: SpatialPoint, elevation: float = 0):
-        """Helper function to check if a SpatialPoint is on the land.
-            Accuracy is limited by the resolution of self.grid_dict.
-        Args:
-            point:    SpatialPoint object where to check if it is on land
-        Returns:
-            bool:     True if on land and false otherwise
-        """
-
-        if not (
-            self.casadi_grid_dict["x_range"][0]
-            < point.lon.deg
-            < self.casadi_grid_dict["x_range"][1]
-        ):
-            raise ValueError(
-                f"Point {point} is not in casadi_grid_dict lon range{self.casadi_grid_dict['x_range']}"
-            )
-
-        if not (
-            self.casadi_grid_dict["y_range"][0]
-            < point.lat.deg
-            < self.casadi_grid_dict["y_range"][1]
-        ):
-            raise ValueError(
-                f"Point {point} is not in casadi_grid_dict lat range {self.casadi_grid_dict['y_range']}"
-            )
-
-        return self.get_data_at_point(point) > elevation
-
-    def __del__(self):
-        """Helper function to delete the existing casadi functions."""
-        del self.elevation_func
-        pass
-
-    # Plotting Functions for Bathymetry specifically
-    @staticmethod
-    def plot_data_from_xarray(
-        xarray: xr,
-        var_to_plot: AnyStr = "elevation",
-        vmin: Optional[float] = -6000,
-        vmax: Optional[float] = 6000,
-        alpha: Optional[float] = 1.0,
-        ax: plt.axes = None,
-        fill_nan: bool = True,
-    ) -> matplotlib.pyplot.axes:
-        """Bathymetry specific plotting function to plot the x_array.
-        All other functions build on top of it, it creates the ax object and returns it.
-        Args:
-            time_idx:          time-idx to select from the xarray (only if it has time dimension)
-            xarray:            xarray object containing the grids and data
-            var_to_plot:       a string of the variable to plot
-            vmin:              minimum current magnitude used for colorbar (float)
-            vmax:              maximum current magnitude used for colorbar (float)
-            alpha:             alpha of the current magnitude color visualization
-            ax:                Optional for feeding in an axis object to plot the figure on.
-            fill_nan:          Optional if True we fill nan values with 0 otherwise leave them as nans.
-        Returns:
-            ax                 matplotlib.pyplot.axes object
-        """
-        if fill_nan:
-            xarray = xarray.fillna(0)
-        if ax is None:
-            ax = plt.axes()
-
-        # plot data for the specific variable
-        # if vmax is None:
-        #     vmax = xarray[var_to_plot].max()
-        # if vmin is None:
-        #     vmin = xarray[var_to_plot].min()
-        # TODO: think of smart structure
-        # Fix colorbar limits, as land will be covered by platecarree land map
-        # if we use geographic coordinate system and we don't need it for land
-        xarray[var_to_plot].plot(cmap="cmo.topo", vmin=vmin, vmax=vmax, alpha=alpha, ax=ax)
-        # Label the plot
-        ax.set_title(
-            "Variable: {var} \n at Time: {t}".format(
-                var=var_to_plot, t="Time: " + time.strftime("%Y-%m-%d %H:%M:%S UTC")
-            )
-        )
-        return ax
-
-
-class BathymetrySource:
-    def __init__(
-        self,
-        casadi_cache_dict: Dict,
-        source_dict: Dict,
-        use_geographic_coordinate_system: Optional[bool] = True,
-    ):
-        """Initialize the source objects from the respective settings dicts
-
-        Args:
-            casadi_cache_dict (Dict): containing the cache settings to use in the sources for caching of 3D data
-                          e.g. {'deg_around_x_t': 2}
-            source_dict (Dict): _description_
-            use_geographic_coordinate_system (Optional[bool], optional): _description_. Defaults to True.
-        """
-        self.logger = logging.getLogger("areana.bathymetry_source")
-        self.logger.setLevel(os.environ.get("LOGLEVEL", "INFO").upper())
-        self.DataArray = None  # Xarray containing raw data
-        self.elevation_func = None  # Casadi function
-        self.grid_dict, self.casadi_grid_dict = [None] * 2
         self.source_dict = source_dict
+        self.DataArray = None  # Xarray containing raw data
+        self.grid_dict, self.casadi_grid_dict = [None] * 2
 
         # Step 1: create source
         start = time.time()
-        self.source_dict["casadi_cache_settings"] = casadi_cache_dict
-        self.source_dict["use_geographic_coordinate_system"] = use_geographic_coordinate_system
-        self.instantiate_source_from_dict(source_dict)
-        self.logger.info(f"BathymetrySource: Create source({time.time() - start:.1f}s)")
+        self.instantiate_source_from_dict()
+        self.logger.info(f"DataSource2d: Create source({time.time() - start:.1f}s)")
 
-    def instantiate_source_from_dict(self, source_dict: dict):
-        if source_dict["source"] == "gebco":
-            self.DataArray = self.get_bathymetry_from_file()
-            self.grid_dict = self.get_grid_dict_from_xr(self.DataArray)
-        else:
-            raise NotImplementedError(
-                f"Selected source {source_dict['source']} in the BathymetrySource dict is not implemented."
-            )
+    @abc.abstractmethod
+    def instantiate_source_from_dict(self) -> None:
+        """Instantiate DataArray and grid_dict"""
+        pass
 
-    def get_bathymetry_from_file(self) -> xr:
-        DataArray = xr.open_dataset(self.source_dict["source_settings"]["filepath"])
-        # DataArray = DataArray.rename({"latitude": "lat", "longitude": "lon"})
-        return DataArray
+    @abc.abstractmethod
+    def get_DataArray_from_file(self) -> xr:
+        """Get the DataArray from file"""
+        pass
 
+    @abc.abstractmethod
     def get_data_at_point(self, spatial_point: SpatialPoint) -> float:
-        # TODO: Watch for order lon, lat!
-        return self.elevation_func(spatial_point.__array__()[::-1])
+        pass
 
+    @abc.abstractmethod
     def initialize_casadi_functions(self, grid: List[List[float]], array: xr) -> None:
         """DataSource specific function to initialize the casadi functions needed.
         # Note: the input to the casadi function needs to be an array of the form np.array([lat, lon])
@@ -195,9 +55,7 @@ class BathymetrySource:
           array:    xarray object containing the sub-setted data for the next cached round
         """
 
-        self.elevation_func = ca.interpolant(
-            "elevation", "linear", grid, array["elevation"].values.ravel(order="F")
-        )
+    pass
 
     def update_casadi_dynamics(self, state: PlatformState) -> None:
         """Function to update casadi_dynamics which means we fit an interpolant to grid data.
@@ -340,38 +198,13 @@ class BathymetrySource:
 
         return lat_bnds, lon_bnds
 
-    def is_higher_than(self, point: SpatialPoint, elevation: float = 0):
-        """Helper function to check if a SpatialPoint is on the land.
-            Accuracy is limited by the resolution of self.grid_dict.
-        Args:
-            point:    SpatialPoint object where to check if it is on land
-        Returns:
-            bool:     True if on land and false otherwise
-        """
+    @abc.abstractmethod
+    def __del__(self):
+        """Helper function to delete the existing casadi functions."""
+        pass
 
-        if not (
-            self.casadi_grid_dict["x_range"][0]
-            < point.lon.deg
-            < self.casadi_grid_dict["x_range"][1]
-        ):
-            raise ValueError(
-                f"Point {point} is not in casadi_grid_dict lon range{self.casadi_grid_dict['x_range']}"
-            )
-
-        if not (
-            self.casadi_grid_dict["y_range"][0]
-            < point.lat.deg
-            < self.casadi_grid_dict["y_range"][1]
-        ):
-            raise ValueError(
-                f"Point {point} is not in casadi_grid_dict lat range {self.casadi_grid_dict['y_range']}"
-            )
-
-        return self.get_data_at_point(point) > elevation
-
-    def plot_data_at_time_over_area(
+    def plot_data_over_area(
         self,
-        time: Union[datetime.datetime, float],
         x_interval: List[float],
         y_interval: List[float],
         spatial_resolution: Optional[float] = None,
@@ -379,9 +212,8 @@ class BathymetrySource:
         ax: Optional[matplotlib.pyplot.axes] = None,
         **kwargs,
     ):
-        """Plot the data at a specific time over an area defined by the x and y intervals.
+        """Plot the data at a specific area defined by the x and y intervals.
         Args:
-          time:             time for which to plot the data either posix or datetime.datetime object
           x_interval:       List of the lower and upper x area in the respective coordinate units [x_lower, x_upper]
           y_interval:       List of the lower and upper y area in the respective coordinate units [y_lower, y_upper]
           spatial_resolution:Per default (None) the data_source resolution is used otherwise the selected one.
@@ -390,10 +222,6 @@ class BathymetrySource:
           **kwargs:         Further keyword arguments for more specific setting, see plot_currents_from_2d_xarray.
         """
 
-        # Format to datetime object
-        if not isinstance(time, datetime.datetime):
-            time = datetime.datetime.fromtimestamp(time, tz=datetime.timezone.utc)
-
         # Step 1: get the area data
         area_xarray = self.get_data_over_area(
             x_interval,
@@ -401,19 +229,25 @@ class BathymetrySource:
             spatial_resolution=spatial_resolution,
         )
 
-        # interpolate to specific time
-        # atTimeArray = area_xarray.interp(time=time.replace(tzinfo=None))
-
         # Plot the current field
-        ax = self.plot_xarray_for_animation(time_idx=0, xarray=area_xarray, ax=ax, **kwargs)
+        ax = self.plot_xarray_for_animation(xarray=area_xarray, ax=ax, **kwargs)
         if return_ax:
             return ax
         else:
             plt.show()
 
+    @staticmethod
+    def set_up_geographic_ax() -> matplotlib.pyplot.axes:
+        """Helper function to set up a geographic ax object to plot on."""
+        ax = plt.axes(projection=ccrs.PlateCarree())
+        grid_lines = ax.gridlines(draw_labels=True, zorder=5)
+        grid_lines.top_labels = False
+        grid_lines.right_labels = False
+        ax.add_feature(cfeature.LAND, zorder=3, edgecolor="black")
+        return ax
+
     def plot_xarray_for_animation(
         self,
-        time_idx: int,
         xarray: xr,
         reset_plot: Optional[bool] = False,
         figsize: Tuple[int] = (6, 6),
@@ -423,7 +257,6 @@ class BathymetrySource:
         """Helper function for animations adding plot resets, figure size and automatically generating the axis.
         See plot_data_from_xarray for other optional keyword arguments.
         Args:
-            time_idx:          time-idx to select from the xarray (only if it has time dimension)
             xarray:            xarray object containing the grids and data
             reset_plot:        if True the current figure is re-setted otherwise a new figure created (used for animation)
             figsize:           figure size
@@ -441,17 +274,16 @@ class BathymetrySource:
         # Step 2: Create ax object
         if ax is None:
             if self.source_dict["use_geographic_coordinate_system"]:
-                # TODO: fix later
-                # ax = self.set_up_geographic_ax()
-                ax = plt.axes()
+                ax = self.set_up_geographic_ax()
             else:
                 ax = plt.axes()
 
-        return self.plot_data_from_xarray(time_idx=time_idx, xarray=xarray, ax=ax, **kwargs)
+        return self.plot_data_from_xarray(xarray=xarray, ax=ax, **kwargs)
 
+    # TODO: Decide if abstractmethod makes more sense. If var_to_plot is not provided it seems very fragile
+    # @abc.abstractmethod
     @staticmethod
     def plot_data_from_xarray(
-        time_idx: int,
         xarray: xr,
         var_to_plot: AnyStr = None,
         vmin: Optional[float] = None,
@@ -460,11 +292,9 @@ class BathymetrySource:
         ax: plt.axes = None,
         fill_nan: bool = True,
     ) -> matplotlib.pyplot.axes:
-        """Base function to plot a specific var_to_plot of the x_array. If xarray has a time-dimension time_idx is selected,
-        if xarray's time dimension is already collapsed (e.g. after interpolation) it's directly plotted.
+        """Base function to plot a specific var_to_plot of the x_array.
         All other functions build on top of it, it creates the ax object and returns it.
         Args:
-            time_idx:          time-idx to select from the xarray (only if it has time dimension)
             xarray:            xarray object containing the grids and data
             var_to_plot:       a string of the variable to plot
             vmin:              minimum current magnitude used for colorbar (float)
@@ -478,29 +308,20 @@ class BathymetrySource:
         if fill_nan:
             xarray = xarray.fillna(0)
         # Get data variable if not provided
-        # TODO: fix
         if var_to_plot is None:
-            var_to_plot = "elevation"
+            var_to_plot = list(xarray.keys())[0]
         if ax is None:
             ax = plt.axes()
-
         # plot data for the specific variable
         if vmax is None:
             vmax = xarray[var_to_plot].max()
         if vmin is None:
             vmin = xarray[var_to_plot].min()
-        # TODO: adapt when separated into classes
-        xarray[var_to_plot].plot(cmap="cmo.topo", vmin=-6000, vmax=6000, alpha=alpha, ax=ax)
+        xarray[var_to_plot].plot(cmap="viridis", vmin=vmin, vmax=vmax, alpha=alpha, ax=ax)
         # Label the plot
         ax.set_title(
             "Variable: {var} \n at Time: {t}".format(
                 var=var_to_plot, t="Time: " + time.strftime("%Y-%m-%d %H:%M:%S UTC")
             )
         )
-
         return ax
-
-    def __del__(self):
-        """Helper function to delete the existing casadi functions."""
-        del self.elevation_func
-        pass
---
+++ scripts/tutorial/data_sources/bathymetry_source_2d.py
@@ -0,0 +1,62 @@
+import datetime
+
+from ocean_navigation_simulator.utils.units import Distance
+from ocean_navigation_simulator.environment.PlatformState import (
+    PlatformState,
+    SpatialPoint,
+)
+
+from ocean_navigation_simulator.data_sources.Bathymetry.BathymetrySource import BathymetrySource2d
+import matplotlib.pyplot as plt
+
+# Initialize bathymetry source
+# TODO: check if time_around_x_t is needed
+# casadi_cache_dict = {"deg_around_x_t": 20, "time_around_x_t": 3600 * 24 * 1}
+bathymetry_source_dict = {
+    "field": "Bathymetry",
+    "source": "gebco",
+    "source_settings": {"filepath": "data/bathymetry/bathymetry_global_res_0.083_0.083_max.nc"},
+    "casadi_cache_settings": {"deg_around_x_t": 20},
+    "use_geographic_coordinate_system": True,
+}
+bathymetry_field = BathymetrySource2d(
+    source_dict=bathymetry_source_dict,
+)
+
+
+#%% Check if degree around work and if get_data_at_point_work
+# Expect inaccuracies due to resolution of bathymetry map
+t_0 = datetime.datetime(2021, 11, 25, 23, 30, tzinfo=datetime.timezone.utc)
+# Define Mariannas Trench
+lat = 11.326344
+lon = 142.187248
+mariannas_trench = SpatialPoint(lat=Distance(deg=lat), lon=Distance(deg=lon))
+x_0 = PlatformState(lon=Distance(deg=lon - 10), lat=Distance(deg=lat + 10), date_time=t_0)
+bathymetry_field.update_casadi_dynamics(x_0)
+print(f"Elevation at {mariannas_trench} is {bathymetry_field.get_data_at_point(mariannas_trench)}")
+
+# Define Mount Everest
+lat = 27.9881
+lon = 86.9250
+everest = SpatialPoint(lat=Distance(deg=lat), lon=Distance(deg=lon))
+x_0 = PlatformState(lon=Distance(deg=lon - 10), lat=Distance(deg=lat + 10), date_time=t_0)
+bathymetry_field.update_casadi_dynamics(x_0)
+print(f"Elevation at {everest} is {bathymetry_field.get_data_at_point(everest)}")
+
+print(f"Everest is higher than 8000m: {bathymetry_field.is_higher_than(everest, 8000)}")
+# We can also check if casadi loaded this area (This should fail)
+# print(
+#     f"Mariannas Trench is higher than -9000m: {bathymetry_field.is_higher_than(mariannas_trench, -9000)}"
+# )
+
+#%% Plot bathymetry over full field
+# TODO: possibly need time as required by higher level function?
+# World
+ax = bathymetry_field.plot_data_over_area(
+    bathymetry_field.grid_dict["x_range"], bathymetry_field.grid_dict["y_range"]
+)
+plt.show()
+# Region 1
+ax = bathymetry_field.plot_data_over_area([-160, -105], [15, 40])
+plt.show()
+print("Hurray")
