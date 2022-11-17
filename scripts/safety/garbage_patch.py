7a4c47f1e0d72ed649bebf6b42b09cfae10156a2
Andreas <35858164+a-doering@users.noreply.github.com>
2022-11-17 14:47:59 -0800

    Add alpha complex and saving

--- scripts/safety/garbage_patch.py
+++ scripts/safety/garbage_patch.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import AnyStr, List, Optional
 
 import cartopy.crs as ccrs
@@ -10,8 +11,6 @@ import xarray as xr
 from scipy.spatial import ConvexHull, Delaunay
 
 
-# TODO: replace with the function form datasource
-# @staticmethod
 def set_up_geographic_ax() -> plt.axes:
     """Helper function to set up a geographic ax object to plot on."""
     ax = plt.axes(projection=ccrs.PlateCarree())
@@ -92,13 +91,21 @@ def plot_alpha_complex(points, ax, alpha):
     plt.plot(points[:, 0], points[:, 1], ".")
     for i, j in edges:
         plt.plot(points[[i, j], 0], points[[i, j], 1])
-    plt.text(270.5, 459, "alpha=1", size=18)
+    plt.text(270.5, 459, f"alpha={alpha}", size=18)
     plt.show()
 
 
 def create_xarray_from_csv(
     filename: str = "data/lebreton/Lebreton2018_HistoricalDataset.csv",
 ) -> xr:
+    """_summary_
+
+    Args:
+        filename (str, optional): Csv file containing garbage patch data. Defaults to "data/lebreton/Lebreton2018_HistoricalDataset.csv".
+
+    Returns:
+        xr: Xarray with points within the GPGP (great pacific garbage patch).
+    """
     df = pd.read_csv(filename)
     df = df.rename(columns={"Latitude (degrees)": "lat", "Longitude (degrees)": "lon"})
     df = df[df["Inside GPGP?"] == 1]
@@ -110,18 +117,65 @@ def create_xarray_from_csv(
 # TODO: only GPGP region or global (both with only GPGP data)?
 # TODO: figure out why alpha complex with alpha value zero does not return the complex hull
 # TODO: there will be errors in areas where the convex hull should wrap around, hence at +-180°
-
-
 def create_xarray_mask(
     xarray: xr,
     x_range: List[float],
     y_range: List[float],
     res_lat: float = 1 / 12,
     res_lon: float = 1 / 12,
+    algorithm: str = "ConvexHull",
+    alpha: float = 2.0,
 ) -> xr:
+    """Create an xarray mask of garbage patches
+
+    Args:
+        xarray (xr): xarray containing lat, lon columns of positions within a garbage patch.
+        x_range (List[float]): List of the lower and upper x area in the respective coordinate units [x_lower, x_upper]
+        y_range (List[float]): List of the lower and upper y area in the respective coordinate units [y_lower, y_upper]
+        res_lat (float, optional): Target resolution lat in degree. Defaults to 1/12.
+        res_lon (float, optional): Target resolution lat in degree. Defaults to 1/12.
+        algorithm (str, optional): Select which algorithm to use to get perimeter around points. Defaults to "ConvexHull".
+        alpha (float, optional): Alpha value if AlphaComplex is chosen as algorithm. Defaults to 2.0.
+
+    Raises:
+        NotImplementedError: If you select an algorithm that is not supported.
+
+    Returns:
+        xr: Xarray with lat, lon, garbage fields with boolean mask of garbage. True for garbage.
+    """
     points = np.column_stack((ds["lon"], ds["lat"]))
-    hull = ConvexHull(points)
-    vertices = points[hull.vertices]
+
+    # Select algorithm
+    if algorithm == "ConvexHull":
+        hull = ConvexHull(points)
+        vertices = points[hull.vertices]
+    elif algorithm == "AlphaComplex":
+        edges = alpha_complex(points, alpha)
+        edges_list = [pair for pair in edges]
+        vertices_indices = [edges_list[0][0]]
+
+        i = 0
+        while len(edges_list):
+            # TODO: this is if there are multiple unconnected alpha complexes. Possible need to debug alpha_complexes
+            i += 1
+            if i > 1000:
+                print("Failure to create connected alpha complexes.")
+                break
+            for pair in edges_list:
+                if vertices_indices[-1] in pair:
+                    if vertices_indices[-1] == pair[0]:
+                        vertices_indices.append(pair[1])
+                    elif vertices_indices[-1] == pair[1]:
+                        vertices_indices.append(pair[0])
+                    edges_list.remove(pair)
+                else:
+                    continue
+
+        vertices = points[vertices_indices]
+        # raise NotImplementedError("AlphaComplex WIP, select Complex hull")
+    else:
+        raise NotImplementedError("Select algorithm that is implemented")
+
     polygon = np.array(vertices)
     path = matplotlib.path.Path(polygon)
 
@@ -133,33 +187,37 @@ def create_xarray_mask(
     garbage_mask = path.contains_points(points_in_area)
     garbage_mask.shape = xv.shape
     # plt.imshow(garbage_mask, origin="lower")  # ,  # interpolation="nearest")
-
     # Remapping from mask values to lat, lon
     xarray_masked = xr.Dataset(
         dict(garbage=(["lat", "lon"], garbage_mask)), coords=dict(lon=new_lon, lat=new_lat)
     )
-
-    # Test:
-    # xarray_masked["garbage"].plot()
-
     return xarray_masked
 
 
-def save_xarray(
-    xarray: xr, res_lat: float = 1 / 12, res_lon: float = 1 / 12, filename: Optional[AnyStr] = None
-) -> None:
-    if filename is None:
-        filename = f"data/garbage_patch/garbage_patch_res_{res_lat:.3f}_{res_lon:.3f}.nc"
+def save_xarray(xarray: xr, filename: str) -> None:
+    """Save xarray to disk
+
+    Args:
+        xarray (xr): xarray
+        filename (str): filename with path, dirs will be created if not existing.
+    """
+    Path(filename).parent.mkdir(parents=True, exist_ok=True)
     xarray.to_netcdf(filename)
-    print("Saved garbage patch map")
+    print(f"Saved garbage patch mask at {filename}")
 
 
 if __name__ == "__main__":
+    region_1 = [[-175, -100], [15, 45]]
+    res_lat = 1 / 12
+    res_lon = 1 / 12
     ds = create_xarray_from_csv()
-    ds_map = create_xarray_mask(ds, [-175, -100], [15, 45])
-    ds_map["garbage"].plot()
-    # save_xarray(ds_map)
+    xarray = create_xarray_mask(ds, *region_1, res_lat=res_lat, res_lon=res_lon)
+    save_xarray(
+        xarray, f"data/garbage_patch/garbage_patch_region_1_res_{res_lat:.3f}_{res_lon:.3f}.nc"
+    )
 
+    # xarray["garbage"].plot()
+    # plt.show()
     # # # Plot a convex hull around garbage patch
     # ax = set_up_geographic_ax()
     # points = np.column_stack((ds["lon"], ds["lat"]))
