f104434beff536b4ccfa41cc811b4999977fc5d9
Jonas Dieker <jonasdieker05@gmail.com>
2022-12-12 21:23:21 +0100

    add comments and make more general

--- ocean_navigation_simulator/generative_error_model/data_files/copernicus_hindcast_downloader.py
+++ ocean_navigation_simulator/generative_error_model/data_files/copernicus_hindcast_downloader.py
@@ -1,5 +1,4 @@
 from ocean_navigation_simulator.environment.data_sources.OceanCurrentField import OceanCurrentField
-from ocean_navigation_simulator.generative_error_model.utils import load_config, get_path_to_project
 
 import datetime
 import os
@@ -8,19 +7,28 @@ import numpy as np
 from typing import List
 
 
+hindcast_dict = {"field": "OceanCurrents",
+                 "source": "opendap",
+                 "source_settings": {"service": "copernicus",
+                                     "currents": "total",
+                                     "USERNAME": "jdieker",
+                                     "PASSWORD": "AxxzVqCuC#!vS69",
+                                     "DATASET_ID": "cmems_mod_glo_phy_anfc_merged-uv_PT1H-i"}
+                 }
+
+sim_cache_dict = {"deg_arount_x_t": 1, "time_around_x_t": 86400}
+
+
 def get_copernicus_hindcast(lon_range: List[float], lat_range: List[float], start: datetime.datetime,
                             days:int, save_dir: str):
+    """Gets 9 day long copernicus hindcasts for each day specified. E.g. if days=2, it saves two files of length 9
+    days, the second one starting one day after the first.
+    Note 1: Uses Marius' HindcastOpendapSource class which changes the internal xarray naming, this func changes it
+    back by creating a new xarray and saving it -> consider changing this!
+    Note 2: The overlapping 8 days are exactly the same for copernicus hindcast -> consider changing this!"""
 
-    relative_path = "scenarios/generative_error_model/config_buoy_data.yaml"
-    config_path = os.path.join(get_path_to_project(os.getcwd()), relative_path)
-    config = load_config(config_path)
-
-    hindcast_dict = config["copernicus_opendap"]["hindcast"]
-    sim_cache_dict = config["sim_cache_dict"]
-
-    root_dir = os.path.join(get_path_to_project(os.getcwd()), save_dir)
-    if not os.path.exists(root_dir):
-        os.makedirs(root_dir)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
 
     for day in range(days):
         file_start = start + datetime.timedelta(days=day)
@@ -28,12 +36,14 @@ def get_copernicus_hindcast(lon_range: List[float], lat_range: List[float], star
         print(f"Downloading for time period of [{file_start}, {file_end}].")
         file_name = f"copernicus_hindcast_lon_{lon_range}_lat_{lat_range}_time_" + \
                     f"[{start + datetime.timedelta(days=day)},{start + datetime.timedelta(days=9 + day)}].nc"
-        if os.path.exists(os.path.join(root_dir, file_name)):
+        # check if file already exists
+        if os.path.exists(os.path.join(save_dir, file_name)):
             print(f"File already exists!")
             continue
 
-        # need to run OceanCurrentField again so connection does not time out
+        # need to run OceanCurrentField again so connection does not time out -> makes loop very slow
         ocean_field = OceanCurrentField(hindcast_source_dict=hindcast_dict, sim_cache_dict=sim_cache_dict)
+        print("Made connection to Copernicus!")
         # get xarray
         ds_hindcast = ocean_field.hindcast_data_source.DataArray
         # slice for specific range
@@ -45,6 +55,7 @@ def get_copernicus_hindcast(lon_range: List[float], lat_range: List[float], star
         water_v = hindcast["water_v"].values[:, np.newaxis, :, :]
         attrs = hindcast.attrs
 
+        # Need to create new xarray to change data variable names back to be the same as copernicus forecast
         hindcast = xr.Dataset(
             data_vars=dict(
                 utotal=(["time", "depth", "latitude", "longitude"], water_u),
@@ -59,17 +70,15 @@ def get_copernicus_hindcast(lon_range: List[float], lat_range: List[float], star
             attrs=attrs
         )
         # save data to file
-        hindcast.to_netcdf(os.path.join(root_dir, file_name), engine="netcdf4")
+        hindcast.to_netcdf(os.path.join(save_dir, file_name), engine="netcdf4")
         print(f"Written: {file_name}.")
     print("Finished downloads.")
 
 
 if __name__ == "__main__":
-    save_dir = "data/drifter_data/hindcasts/"
+    save_dir = "/home/jonas/Downloads"
     lon_range = [-150, -115]
     lat_range = [0, 40]
-    # lon_range = [-120, -90]
-    # lat_range = [-15, 15]
     start = datetime.datetime(2022, 10, 1, 12, 0, 0)
-    days = 7
+    days = 1
     get_copernicus_hindcast(lon_range, lat_range, start, days, save_dir=save_dir)
