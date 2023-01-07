f88c3c9eb02e8b87896d0554312f0a042e31ef7f
Andreas <35858164+a-doering@users.noreply.github.com>
2023-01-07 15:42:49 -0800

    Add parameters to set base rectangle region

--- scripts/safety/find_areas.py
+++ scripts/safety/find_areas.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import List
 
 import cmocean
 import matplotlib.pyplot as plt
@@ -7,11 +8,6 @@ import xarray as xr
 from ocean_navigation_simulator.data_sources.OceanCurrentField import (
     OceanCurrentField,
 )
-from ocean_navigation_simulator.environment.PlatformState import (
-    PlatformState,
-    SpatialPoint,
-)
-from ocean_navigation_simulator.utils import units
 from scripts.safety.bathymetry import plot_rectangles_from_interval
 
 
@@ -61,12 +57,10 @@ def plot_currents_in_area(area: str):
         x_interval = [-107, -105]
         y_interval = [19, 22]
         output_name = "region1_puertoVallarta_hindcast_current_animation.mp4"
-    # x_0 = PlatformState(lon=units.Distance(deg=-130), lat=units.Distance(deg=20), date_time=t_0)
-    # x_T = SpatialPoint(lon=units.Distance(deg=-140), lat=units.Distance(deg=35))
     t_interval = [t_0, t_0 + datetime.timedelta(days=5)]
     kwargs = {"vmax": 1, "vmin": 0}
 
-    ax = ocean_field.hindcast_data_source.plot_data_at_time_over_area(
+    _ = ocean_field.hindcast_data_source.plot_data_at_time_over_area(
         time=t_0,
         x_interval=x_interval,
         y_interval=y_interval,
@@ -75,60 +69,72 @@ def plot_currents_in_area(area: str):
         return_ax=True,
         **kwargs
     )
-    folder_to_save_in = "generated_media/" if not output_name.startswith("/") else ""
+    folder_to_save_in = "generated_media/region1_areas/" if not output_name.startswith("/") else ""
     plt.savefig(folder_to_save_in + output_name[:-3] + "png")
 
     ocean_field.hindcast_data_source.animate_data(
         x_interval=x_interval,
         y_interval=y_interval,
         t_interval=t_interval,
-        output=output_name,
+        output="region1_areas/" + output_name,
         **kwargs
     )
 
 
-def plot_area_with_rectangles(map_type: str):
+def plot_area_with_rectangles(
+    map_type: str, x_range: List[float] = [-180, -180], y_range: List[float] = [-90, 90]
+):
+    # Set background and range
     if map_type == "bathymetry":
+        # Full
+
         xarray = xr.open_dataset("data/bathymetry/bathymetry_global_res_0.083_0.083_max.nc")
-        p = xarray["elevation"].plot(cmap=cmocean.cm.delta, vmin=-6000, vmax=6000)
+        p = (
+            xarray["elevation"]
+            .sel(
+                lat=slice(y_range[0], y_range[1]),
+                lon=slice(x_range[0], x_range[1]),
+            )
+            .plot(cmap=cmocean.cm.delta, vmin=-150, vmax=150)
+        )
 
     # t_0 = datetime.datetime(2022, 10, 10, 13, 00, tzinfo=datetime.timezone.utc)
 
     # Full
     x_interval = [-160, -105]
     y_interval = [15, 40]
-    ax = plot_rectangles_from_interval(x_interval, y_interval, p.axes, color="r")
+    _ = plot_rectangles_from_interval(x_interval, y_interval, p.axes, color="r")
 
     # SF
     x_interval = [-125, -121]
     y_interval = [36, 40]
-    ax = plot_rectangles_from_interval(x_interval, y_interval, p.axes)
+    _ = plot_rectangles_from_interval(x_interval, y_interval, p.axes)
 
     # LA
     x_interval = [-121, -117]
     y_interval = [32, 35]
-    ax = plot_rectangles_from_interval(x_interval, y_interval, p.axes)
+    _ = plot_rectangles_from_interval(x_interval, y_interval, p.axes)
 
     # Hawaii -> Just outside of this, but still in region 1
     x_interval = [-160, -152]
     y_interval = [18, 23]
-    ax = plot_rectangles_from_interval(x_interval, y_interval, p.axes)
+    _ = plot_rectangles_from_interval(x_interval, y_interval, p.axes)
 
     # Baja california, east side channel
     x_interval = [-114, -111]
     y_interval = [28, 30]
-    ax = plot_rectangles_from_interval(x_interval, y_interval, p.axes)
+    _ = plot_rectangles_from_interval(x_interval, y_interval, p.axes)
 
     # Puerto Vallarta, Mexico mainland south east of baja california
     x_interval = [-107, -105]
     y_interval = [19, 22]
-    ax = plot_rectangles_from_interval(x_interval, y_interval, p.axes)
+    _ = plot_rectangles_from_interval(x_interval, y_interval, p.axes)
     plt.show()
 
 
 if __name__ == "__main__":
-    map_type = "bathymetry"
-    # plot_area_with_rectangles(map_type)
+    # Region1
+    plot_area_with_rectangles("bathymetry", [-160, -105], [15, 40])
 
     areas = ["sf", "la", "hawaii", "bajaNE", "puertoVallarta"]
     for area in areas:
