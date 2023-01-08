b0b04100ab528e948a047afb235fdbf0109fea46
Andreas <35858164+a-doering@users.noreply.github.com>
2023-01-07 18:22:28 -0800

    Adapt naive safety controller to work with multiple types of areas to avoid

--- ocean_navigation_simulator/controllers/NaiveSafetyController.py
+++ ocean_navigation_simulator/controllers/NaiveSafetyController.py
@@ -11,7 +11,7 @@ import xarray as xr
 
 
 class NaiveSafetyController(Controller):
-    """Naive safety controller. Steers in direction furthest away from land.
+    """Naive safety controller. Steers in direction furthest away from area.
     Can end up in local maxima."""
 
     gpus: float = 0.0
@@ -19,20 +19,31 @@ class NaiveSafetyController(Controller):
     def __init__(self, problem: NavigationProblem, specific_settings: dict) -> None:
         super().__init__(problem)
         self.specific_settings = specific_settings
-        self.distance_map = xr.open_dataset(self.specific_settings["filepath_distance_map"])
+        self.distance_map = dict()
+        self.distance_map["bathymetry"] = xr.open_dataset(
+            self.specific_settings["filepath_distance_map"]["bathymetry"]
+        )
+        self.distance_map["garbage"] = xr.open_dataset(
+            self.specific_settings["filepath_distance_map"]["garbage"]
+        )
 
-    def find_bearing_to_max_distance_to_land(
-        self, observation: ArenaObservation, num_directions_to_sample=16, radius_in_deg: float = 0.1
+    def find_bearing_to_max_distance_to_area(
+        self,
+        observation: ArenaObservation,
+        area_type: str = "bathymetry",
+        num_directions_to_sample=16,
+        radius_in_deg: float = 0.1,
     ) -> float:
-        """Find bearing to move platform furthest away from land.
+        """Find bearing to move platform furthest away from area.
 
         Args:
             observation (ArenaObservation): Current observation.
+            area_type (str): Type of area to stay away from, e.g. "garbage", "bathymetry"
             num_directions_to_sample (int, optional): Number of directions to sample around platform. Defaults to 16.
             radius_in_deg (float, optional): Radius in degree to the samples. Defaults to 0.1.
 
         Returns:
-            float: Bearing in degree to get the furthest away from land.
+            float: Bearing in degree to get the furthest away from area.
         """
         bearing = np.linspace(0, 360, num_directions_to_sample, endpoint=False)
         # TODO: numpyify
@@ -44,23 +55,28 @@ class NaiveSafetyController(Controller):
             for b in np.deg2rad(bearing)
         ]
 
-        min_d_to_land = [
-            self.distance_map.interp(lon=p[0], lat=p[1])["distance"].data for p in sampled_positions
+        # TODO: Try if this map is available?
+        min_d_to_area = [
+            self.distance_map[area_type].interp(lon=p[0], lat=p[1])["distance"].data
+            for p in sampled_positions
         ]
-        bearing = bearing[np.argmax(min_d_to_land)]
+        bearing = bearing[np.argmax(min_d_to_area)]
 
         return bearing
 
-    def get_action(self, observation: ArenaObservation) -> PlatformAction:
-        """Get action to actuate in the direction furthest away from land.
+    def get_action(
+        self, observation: ArenaObservation, area_type: str = "bathymetry"
+    ) -> PlatformAction:
+        """Get action to actuate in the direction furthest away from area.
 
         Args:
             observation (ArenaObservation): Current Arena observation.
+            area_type (str): Type of area to stay away from, e.g. "garbage", "bathymetry"
 
         Returns:
             PlatformAction: Which action to take.
         """
-        bearing = self.find_bearing_to_max_distance_to_land(observation)
+        bearing = self.find_bearing_to_max_distance_to_area(observation, area_type)
         dlon = np.cos(np.deg2rad(bearing))
         dlat = np.sin(np.deg2rad(bearing))
         mag = math.sqrt(dlon**2 + dlat**2)
--- scripts/tutorial/safety/naive_safety_controller_garbage.py
+++ scripts/tutorial/safety/naive_safety_controller_garbage.py
@@ -1,4 +1,5 @@
 import datetime
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 from ocean_navigation_simulator.controllers.NaiveSafetyController import NaiveSafetyController
@@ -14,17 +15,20 @@ from ocean_navigation_simulator.utils import units
 
 
 # Initialize the Arena (holds all data sources and the platform, everything except controller)
-arena = ArenaFactory.create(scenario_name="safety_gulf_of_mexico_HYCOM_hindcast_local")
+arena = ArenaFactory.create(
+    scenario_name="safety_region1_Copernicus_forecast_Copernicus_hindcast_local"
+)
 
 # -87.5, 29, should go directly down, did it and lead to more or less canceling out the currents
 # -83, 28.5, near florida west coast, should go to middle, with lots of actuation it does, with 0.1m/s it is not moving much
 # -83.11, 29.0 current directly onshore, goes away from shore and saves the day. Alternative is to use PassiveFloating Controller to see stranding.
 x_0 = PlatformState(
-    lon=units.Distance(deg=-83),
-    lat=units.Distance(deg=28.95),
-    date_time=datetime.datetime(2021, 11, 24, 12, 0, tzinfo=datetime.timezone.utc),
+    lon=units.Distance(deg=-158.5),  # -118.5
+    lat=units.Distance(deg=28.4),  # 32.8
+    date_time=datetime.datetime(2022, 10, 10, 14, 0, tzinfo=datetime.timezone.utc),
 )
-x_T = SpatialPoint(lon=units.Distance(deg=-80.3), lat=units.Distance(deg=24.6))
+
+x_T = SpatialPoint(lon=units.Distance(deg=-158.1), lat=units.Distance(deg=27.9))  # -118, 33
 
 problem = NavigationProblem(
     start_state=x_0,
@@ -32,15 +36,29 @@ problem = NavigationProblem(
     target_radius=0.1,
 )
 
+# Not necessary, but good to visualize
 t_interval, lat_bnds, lon_bnds = arena.ocean_field.hindcast_data_source.convert_to_x_y_time_bounds(
-    x_0=x_0.to_spatio_temporal_point(), x_T=x_T, deg_around_x0_xT_box=1, temp_horizon_in_s=3600
+    x_0=x_0.to_spatio_temporal_point(),
+    x_T=x_T,
+    deg_around_x0_xT_box=1,
+    temp_horizon_in_s=3600,
 )
 
 ax = arena.ocean_field.hindcast_data_source.plot_data_at_time_over_area(
     time=x_0.date_time, x_interval=lon_bnds, y_interval=lat_bnds, return_ax=True
 )
-# problem.plot(ax=ax)
-# plt.show()
+ax = arena.garbage_source.plot_mask_from_xarray(
+    xarray=arena.garbage_source.get_data_over_area(x_interval=lon_bnds, y_interval=lat_bnds),
+    var_to_plot="garbage",
+    contour=True,
+    hatches="///",
+    overlay=False,
+    ax=ax,
+)
+problem.plot(ax=ax)
+plt.show()
+##
+
 
 specific_settings = {
     # "d_land_min": 20,
@@ -52,13 +70,14 @@ specific_settings = {
 planner = NaiveSafetyController(problem, specific_settings)
 # % Run reachability planner
 observation = arena.reset(platform_state=x_0)
-action = planner.get_action(observation=observation)
 #%% Let controller run close-loop within the arena
 for i in tqdm(range(int(3600 * 24 * 3 / 600))):  # 5 days
-    action = planner.get_action(observation=observation)
+    action = planner.get_action(observation=observation, area_type="garbage")
     observation = arena.step(action)
 
 #%% Plot the arena trajectory on the map
-arena.plot_all_on_map(problem=problem, background="bathymetry")
+arena.plot_all_on_map(problem=problem, background="garbage")
+arena.plot_all_on_map(problem=problem)
+
 #%% Animate the trajectory
-arena.animate_trajectory(problem=problem, temporal_resolution=7200)
+larena.animate_trajectory(problem=problem, temporal_resolution=7200)
