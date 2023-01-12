0f2913b959eb15e362a5bbb9dce82a5760592f29
Andreas <35858164+a-doering@users.noreply.github.com>
2023-01-11 17:09:44 -0800

    Change config structure

--- scripts/tutorial/safety/safety_region1.py
+++ scripts/tutorial/safety/safety_region1.py
@@ -12,10 +12,43 @@ from ocean_navigation_simulator.environment.Platform import PlatformState
 from ocean_navigation_simulator.environment.PlatformState import SpatialPoint
 from ocean_navigation_simulator.utils import units
 import matplotlib.pyplot as plt
-from config.safety.config_switching_controller import (
-    specific_settings_switching,
-    specific_settings_safety,
-    specific_settings_navigation,
+
+specific_settings_navigation = {
+    "replan_on_new_fmrc": True,
+    "replan_every_X_seconds": False,
+    "direction": "backward",
+    "n_time_vector": 200,
+    # Note that this is the number of time-intervals, the vector is +1 longer because of init_time
+    "deg_around_xt_xT_box": 1.0,  # area over which to run HJ_reachability
+    "accuracy": "high",
+    "artificial_dissipation_scheme": "local_local",
+    "T_goal_in_seconds": 3600 * 24 * 1,
+    "use_geographic_coordinate_system": True,
+    "progress_bar": True,
+    "initial_set_radii": [
+        0.1,
+        0.1,
+    ],  # this is in deg lat, lon. Note: for Backwards-Reachability this should be bigger.
+    # Note: grid_res should always be bigger than initial_set_radii, otherwise reachability behaves weirdly.
+    "grid_res": 0.02,  # Note: this is in deg lat, lon (HYCOM Global is 0.083 and Mexico 0.04)
+    "d_max": 0.0,
+    # 'EVM_threshold': 0.3 # in m/s error when floating in forecasted vs sensed currents
+    # 'fwd_back_buffer_in_seconds': 0.5,  # this is the time added to the earliest_to_reach as buffer for forward-backward
+    # TODO: This is from the config. So we can perhaps pass the whole config and then pick what we need
+    "platform_dict": None,
+}
+
+specific_settings_safety = {
+    "filepath_distance_map": {
+        "bathymetry": "data/bathymetry/bathymetry_distance_res_0.083_0.083_max.nc",
+        "garbage": "data/garbage_patch/garbage_patch_distance_res_0.083_0.083_max.nc",
+    }
+}
+specific_settings_switching = dict(
+    safety_condition={"base_setting": "on", "area_type": ["bathymetry", "garbage"]},
+    safe_distance={"bathymetry": 10, "garbage": 10},  # TODO: change to units,
+    safety_controller="ocean_navigation_simulator.controllers.NaiveSafetyController.NaiveSafetyController(problem, self.specific_settings_safety)",
+    navigation_controller="ocean_navigation_simulator.controllers.hj_planners.HJReach2DPlanner.HJReach2DPlanner(problem, self.specific_settings_navigation)",
 )
 
 
@@ -91,7 +124,7 @@ planner.navigation_controller.plot_reachability_snapshot(
     plot_in_h=True,
 )
 #%% Let controller run close-loop within the arena
-for i in tqdm(range(int(3600 * 24 / 600))):  # 5 days
+for i in tqdm(range(int(3600 * 22 / 600))):  # 5 days
     action = planner.get_action(observation=observation)
     observation = arena.step(action)
     # problem_status = arena.problem_status(problem=problem)
@@ -103,7 +136,7 @@ for i in tqdm(range(int(3600 * 24 / 600))):  # 5 days
     #         ctrl_trajectory[0, idx] * np.cos(ctrl_trajectory[1, idx]),  # u_vector
     # IndexError: index 657 is out of bounds for axis 1 with size 657
     # break
-    print("reached")
+    # print("reached")
 
 #%%
 garbage_traj = arena.plot_garbage_trajectory_on_timeaxis()
