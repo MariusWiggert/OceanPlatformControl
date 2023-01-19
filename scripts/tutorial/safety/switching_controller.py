cc718fb87236a9bcc93ce0f8f6870963a04b7d9f
Andreas <35858164+a-doering@users.noreply.github.com>
2023-01-19 01:45:32 -0800

    Change config structure to adapt to new arena and switching controller

--- scripts/tutorial/safety/switching_controller.py
+++ scripts/tutorial/safety/switching_controller.py
@@ -1,8 +1,12 @@
 import datetime
+
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+from ocean_navigation_simulator.controllers.SwitchingController import (
+    SwitchingController,
+)
 
-# from ocean_navigation_simulator.controllers.PassiveFloatingController import PassiveFloatController
 from ocean_navigation_simulator.environment.ArenaFactory import ArenaFactory
 from ocean_navigation_simulator.environment.NavigationProblem import (
     NavigationProblem,
@@ -10,15 +14,61 @@ from ocean_navigation_simulator.environment.NavigationProblem import (
 from ocean_navigation_simulator.environment.Platform import PlatformState
 from ocean_navigation_simulator.environment.PlatformState import SpatialPoint
 from ocean_navigation_simulator.utils import units
-from ocean_navigation_simulator.controllers.SwitchingController import SwitchingController
 
 
-# Initialize the Arena (holds all data sources and the platform, everything except controller)
-arena = ArenaFactory.create(scenario_name="safety_gulf_of_mexico_HYCOM_hindcast_local")
+scenario_config = {
+    "casadi_cache_dict": {"deg_around_x_t": 0.5, "time_around_x_t": 36000.0},
+    "platform_dict": {
+        "battery_cap_in_wh": 400.0,
+        "u_max_in_mps": 0.1,
+        "motor_efficiency": 1.0,
+        "solar_panel_size": 0.5,
+        "solar_efficiency": 0.2,
+        "drag_factor": 675.0,
+        "dt_in_s": 600.0,
+    },
+    "use_geographic_coordinate_system": True,
+    "spatial_boundary": None,
+    "timeout": 259200,
+    "ocean_dict": {
+        "region": "GOM",
+        "hindcast": {
+            "field": "OceanCurrents",
+            "source": "hindcast_files",
+            "source_settings": {
+                "local": True,
+                "folder": "data/tests/test_BathymetrySource/",
+                "source": "HYCOM",
+                "type": "hindcast",
+            },
+        },
+        "forecast": None,
+    },
+    "bathymetry_dict": {
+        "field": "Bathymetry",
+        "source": "gebco",
+        "source_settings": {
+            "filepath": "ocean_navigation_simulator/package_data/bathymetry_and_garbage/bathymetry_global_res_0.083_0.083_max.nc"
+        },
+        "casadi_cache_settings": {"deg_around_x_t": 20},
+        "use_geographic_coordinate_system": True,
+    },
+    "garbage_dict": None,
+    "solar_dict": {"hindcast": None, "forecast": None},
+    "seaweed_dict": {"hindcast": None, "forecast": None},
+}
+t_interval = [datetime.datetime(2021, 11, 24, 0, 0, 0), datetime.datetime(2021, 12, 5, 0, 0, 0)]
 
-# -87.5, 29, should go directly down, did it and lead to more or less canceling out the currents
-# -83, 28.5, near florida west coast, should go to middle, with lots of actuation it does, with 0.1m/s it is not moving much
-# -83.11, 29.0 current directly onshore, goes away from shore and saves the day. Alternative is to use PassiveFloating Controller to see stranding.
+# # Download files if not there yet
+# ArenaFactory.download_required_files(
+#     archive_source="HYCOM",
+#     archive_type="hindcast",
+#     region="GOM",
+#     download_folder=scenario_config["ocean_dict"]["hindcast"]["source_settings"]["folder"],
+#     t_interval=t_interval,
+# )
+arena = ArenaFactory.create(scenario_config=scenario_config)
+
 x_0 = PlatformState(
     lon=units.Distance(deg=-83),
     lat=units.Distance(deg=28.95),
@@ -31,7 +81,7 @@ problem = NavigationProblem(
     end_region=x_T,
     target_radius=0.1,
 )
-
+#%% Additional plots for better visualization
 t_interval, lat_bnds, lon_bnds = arena.ocean_field.hindcast_data_source.convert_to_x_y_time_bounds(
     x_0=x_0.to_spatio_temporal_point(), x_T=x_T, deg_around_x0_xT_box=1, temp_horizon_in_s=3600
 )
@@ -39,10 +89,15 @@ t_interval, lat_bnds, lon_bnds = arena.ocean_field.hindcast_data_source.convert_
 ax = arena.ocean_field.hindcast_data_source.plot_data_at_time_over_area(
     time=x_0.date_time, x_interval=lon_bnds, y_interval=lat_bnds, return_ax=True
 )
-# problem.plot(ax=ax)
-# plt.show()
+# Many more options to customize the visualization
+ax = arena.bathymetry_source.plot_mask_from_xarray(
+    xarray=arena.bathymetry_source.get_data_over_area(x_interval=lon_bnds, y_interval=lat_bnds),
+    ax=ax,
+)
+problem.plot(ax=ax)
+plt.show()
 
-
+#%% Prepare controller
 specific_settings_navigation = {
     "replan_on_new_fmrc": True,
     "replan_every_X_seconds": False,
@@ -67,47 +122,60 @@ specific_settings_navigation = {
     # TODO: This is from the config. So we can perhaps pass the whole config and then pick what we need
     "platform_dict": arena.platform.platform_dict,
 }
-
-specific_settings_safety = dict(
-    filepath_distance_map="data/bathymetry/bathymetry_distance_res_0.083_0.083_max.nc",
-)
-specific_settings_switching = dict(
-    safety_condition="distance",
-    safe_distance_to_land=20,  # TODO: change to units,
-    safety_controller="ocean_navigation_simulator.controllers.NaiveSafetyController.NaiveSafetyController(problem, self.specific_settings_safety)",
-    navigation_controller="ocean_navigation_simulator.controllers.hj_planners.HJReach2DPlanner.HJReach2DPlanner(problem, self.specific_settings_navigation)",
-)
+specific_settings_safety = {
+    "filepath_distance_map": {
+        "bathymetry": "ocean_navigation_simulator/package_data/bathymetry_and_garbage/bathymetry_distance_res_0.083_0.083_max_elevation_0.nc",
+        "garbage": "ocean_navigation_simulator/package_data/bathymetry_and_garbage/garbage_patch_distance_res_0.083_0.083_max.nc",
+    }
+}
+specific_settings_switching = {
+    "safety_condition": {"base_setting": "on", "area_type": ["bathymetry"]},
+    "safe_distance": {
+        "bathymetry": 20
+    },  # TODO: change to units, currently km. Missiongenerator uses mostly degrees
+    "safety_controller": "ocean_navigation_simulator.controllers.NaiveSafetyController.NaiveSafetyController",
+    "navigation_controller": "ocean_navigation_simulator.controllers.hj_planners.HJReach2DPlanner.HJReach2DPlanner",
+}
 
 planner = SwitchingController(
     problem, specific_settings_switching, specific_settings_navigation, specific_settings_safety
 )
 
-
-# % Run reachability planner
+#%% Run reachability planner
 observation = arena.reset(platform_state=x_0)
 # TODO: this needs to be done to calculate the HJ dynamics at least once
 # In case that the safety is on
-plaction = planner.get_action(observation=observation)
+action = planner.get_action(observation=observation)
 action_nav = planner.navigation_controller.get_action(observation=observation)
 
-
-planner.navigation_controller.plot_reachability_snapshot(
+ax = planner.navigation_controller.plot_reachability_snapshot(
     rel_time_in_seconds=0,
     granularity_in_h=5,
     alpha_color=1,
     time_to_reach=False,
     fig_size_inches=(12, 12),
     plot_in_h=True,
+    return_ax=True,
 )
+# Many more options to customize the visualization
+ax = arena.bathymetry_source.plot_mask_from_xarray(
+    xarray=arena.bathymetry_source.get_data_over_area(x_interval=lon_bnds, y_interval=lat_bnds),
+    ax=ax,
+)
+plt.show()
+
 #%% Let controller run close-loop within the arena
-for i in tqdm(range(int(3600 * 24 * 3 / 600))):  # 5 days
+for i in tqdm(range(int(3600 * 24 * 3 / 600))):  # 3 days
     action = planner.get_action(observation=observation)
     observation = arena.step(action)
 
 #%% Plot the arena trajectory on the map
-arena.plot_all_on_map(problem=problem)
-print("Need to do to save images...")
-#%% Animate the trajectory
-arena.animate_trajectory(problem=problem, temporal_resolution=7200)
-
-print("Done")
+arena.plot_all_on_map(problem=problem, background="bathymetry")  # TODO: fix min, max depth
+#%% Animate the trajectory with bathymetry overlay
+arena.animate_trajectory(
+    add_ax_func_ext=arena.add_ax_func_ext_overlay,
+    problem=problem,
+    temporal_resolution=7200,
+    # output="traj_animation.mp4",
+    **{"masking_val_bathymetry": 0}
+)
