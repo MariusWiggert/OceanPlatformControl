3dd565306cc76301e9339f354659f6af2fd3fad5
Nicolashoi <74150596+Nicolashoi@users.noreply.github.com>
2023-01-20 11:32:05 -0800

    Update __init__.py

--- ocean_navigation_simulator/__init__.py
+++ ocean_navigation_simulator/__init__.py
@@ -6,7 +6,6 @@ from ocean_navigation_simulator import (
     problem_factories,
     utils,
 )
-
 # specify version (for pip installation)
 __version__ = "0.1.1"
-#__all__ = ("controllers", "environment", "ocean_observer", "problem_factories", "utils")
+__all__ = ("controllers", "environment", "ocean_observer", "problem_factories", "utils")
