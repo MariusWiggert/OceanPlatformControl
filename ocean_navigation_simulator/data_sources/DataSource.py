7fcfa552e01978b62dd88345bde4e7a5176befd6
matthiaskiller <matthias_k@gmx.net>
2023-02-23 18:00:26 -0800

    increase padding/buffer size to prevent issues in average data sources

--- ocean_navigation_simulator/data_sources/DataSource.py
+++ ocean_navigation_simulator/data_sources/DataSource.py
@@ -644,7 +644,7 @@ class XarraySource(abc.ABC):
             ]
         # Step 1: Subset the xarray accordingly
         # Step 1.1 include buffers around of the grid granularity (assuming regular grid axis)
-        dt = self.DataArray["time"][1] - self.DataArray["time"][0]
+        dt = self.DataArray["time"][2] - self.DataArray["time"][0]
 
         # Ensure that dt is at least one day since some HYCOM files are missing some timesteps and we have to interpolate on the boundries (does not apply on average data)
         dt = max(dt, np.timedelta64(24, "h"))
