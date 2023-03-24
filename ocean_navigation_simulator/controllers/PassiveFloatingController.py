787ff25e86375ea6e3b8471b1f4c8988592cc0e8
matthiaskiller <matthias_k@gmx.net>
2023-03-24 16:58:31 -0700

    update passiveFloatingController to be conform with constructor

--- ocean_navigation_simulator/controllers/PassiveFloatingController.py
+++ ocean_navigation_simulator/controllers/PassiveFloatingController.py
@@ -1,12 +1,28 @@
+from typing import Dict, Optional
 from ocean_navigation_simulator.controllers.Controller import Controller
-from ocean_navigation_simulator.environment.Arena import ArenaObservation
+from ocean_navigation_simulator.environment.Arena import Arena, ArenaObservation
 from ocean_navigation_simulator.environment.Platform import PlatformAction
+from ocean_navigation_simulator.environment.SeaweedProblem import SeaweedProblem
+from c3python import C3Python
 
 
 class PassiveFloatController(Controller):
     """
     Passively floating controller, always puts out a no propulsion action
     """
+
+    def __init__(
+        self,
+        arena: Arena,
+        problem: SeaweedProblem,
+        specific_settings: Optional[Dict] = ...,
+        c3: Optional[C3Python] = None,
+    ):
+
+        # get arena object for accessing seaweed growth model
+        self.arena = arena
+        self.c3 = c3
+        super().__init__(problem, specific_settings)
 
     def get_action(self, observation: ArenaObservation) -> PlatformAction:
         return PlatformAction.from_xy_propulsion(x_propulsion=0, y_propulsion=0)
