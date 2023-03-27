1f8c910368c569b4f2ade7e3e83a3c29ffcfc049
Andreas <35858164+a-doering@users.noreply.github.com>
2023-03-26 18:18:47 -0700

    Add stranding calculation

--- scripts/archive/statistical_significance_test.py
+++ scripts/archive/statistical_significance_test.py
@@ -8,7 +8,7 @@ N_total = 1151
 count = np.array([N_success_multi, N_success_straight])
 nobs = np.array([N_total, N_total])
 stat, pval = proportions_ztest(count, nobs, alternative="larger")
-print(pval)
+# print(pval)
 #%% Hycom-Copernicus
 N_success_multi = 658  # 689
 N_success_straight = 652
@@ -16,4 +16,43 @@ N_total = 837
 count = np.array([N_success_multi, N_success_straight])
 nobs = np.array([N_total, N_total])
 stat, pval = proportions_ztest(count, nobs, alternative="larger")
+# print(pval)
+
+#%% Stranding
+
+N_total = 1146
+
+# %%
+# switch vs naive
+N_stranding_switch = 29
+N_stranding_hj_naive = 54
+count = np.array([N_stranding_switch, N_stranding_hj_naive])
+nobs = np.array([N_total, N_total])
+stat, pval = proportions_ztest(count, nobs, alternative="smaller")
 print(pval)
+
+# Obs vs naive
+N_stranding_hj_obs = 11
+N_stranding_hj_naive = 54
+count = np.array([N_stranding_hj_obs, N_stranding_hj_naive])
+nobs = np.array([N_total, N_total])
+stat, pval = proportions_ztest(count, nobs, alternative="smaller")
+print(pval)
+
+# switch_obs
+N_stranding_switch = 15
+N_stranding_hj_naive = 54
+count = np.array([N_stranding_switch, N_stranding_hj_naive])
+nobs = np.array([N_total, N_total])
+stat, pval = proportions_ztest(count, nobs, alternative="smaller")
+print(pval)
+
+# obs disturbance
+N_stranding_switch = 22
+N_stranding_hj_naive = 54
+count = np.array([N_stranding_switch, N_stranding_hj_naive])
+nobs = np.array([N_total, N_total])
+stat, pval = proportions_ztest(count, nobs, alternative="smaller")
+print(pval)
+
+# %%
